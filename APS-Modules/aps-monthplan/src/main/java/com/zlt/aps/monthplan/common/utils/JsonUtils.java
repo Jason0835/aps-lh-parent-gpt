7198527baf0e9aaf8e3bedeb8fa40b8dc979b5b5
Abdul.Wu <wushuigen312@163.com>
2025-12-05 18:08:14 +0800

    排产初始化表字段变化

--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/common/utils/JsonUtils.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/common/utils/JsonUtils.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 import com.ruoyi.common.core.web.domain.BaseEntity;
 import com.ruoyi.common.i18n.utils.I18nUtil;
+import com.zlt.aps.common.core.constant.I18nConstant;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -14,18 +15,15 @@ import java.util.List;
 import java.util.Locale;
 
 /**
- * @author xh
+ * 不排产原因等含有国际化信息的解析
+ *
+ * @author ZLT
  * @version 1.0
  * @Description
- * @date 2024/12/2
+ * @date 20251205
  */
 @Slf4j
 public class JsonUtils {
-
-    public static final String CHINESE = "zh_CN";
-
-    public static final String ENGLISH = "en_US";
-
 
     /**
      * 获取语言Json对象
@@ -34,13 +32,12 @@ public class JsonUtils {
      * @return
      */
     public static JSONObject getLanguageJsonObject(String languageKey) {
-        String i18noverModCapsCN = I18nUtil.getMessage(languageKey, Locale.SIMPLIFIED_CHINESE);
-        String i18noverModCapsUS = I18nUtil.getMessage(languageKey, Locale.US);
-
+        String i18nOverModCapsCn = I18nUtil.getMessage(languageKey, Locale.SIMPLIFIED_CHINESE);
+        String i18nOverModCapsUs = I18nUtil.getMessage(languageKey, Locale.US);
         // 构建 JSON 对象
         JSONObject reasonJson = new JSONObject();
-        reasonJson.put(CHINESE, i18noverModCapsCN);
-        reasonJson.put(ENGLISH, i18noverModCapsUS);
+        reasonJson.put(I18nConstant.CHINESE, i18nOverModCapsCn);
+        reasonJson.put(I18nConstant.ENGLISH, i18nOverModCapsUs);
         return reasonJson;
     }
 
@@ -53,15 +50,14 @@ public class JsonUtils {
      * @return
      */
     public static JSONObject getLanguageJsonObject(String languageKey, Object format) {
-        String i18noverModCapsCN = I18nUtil.getMessage(languageKey, Locale.SIMPLIFIED_CHINESE);
-        String i18noverModCapsUS = I18nUtil.getMessage(languageKey, Locale.US);
-
-        String CN = String.format(i18noverModCapsCN, format);
-        String US = String.format(i18noverModCapsUS, format);
+        String i18nOverModCapsCn = I18nUtil.getMessage(languageKey, Locale.SIMPLIFIED_CHINESE);
+        String i18nOverModCapsUs = I18nUtil.getMessage(languageKey, Locale.US);
+        String cn = String.format(i18nOverModCapsCn, format);
+        String us = String.format(i18nOverModCapsUs, format);
         // 构建 JSON 对象
         JSONObject reasonJson = new JSONObject();
-        reasonJson.put(CHINESE, CN);
-        reasonJson.put(ENGLISH, US);
+        reasonJson.put(I18nConstant.CHINESE, cn);
+        reasonJson.put(I18nConstant.ENGLISH, us);
         return reasonJson;
     }
 
@@ -75,15 +71,14 @@ public class JsonUtils {
      * @return
      */
     public static JSONObject getLanguageJsonObject(String languageKey, Object format1, Object format2) {
-        String i18noverModCapsCN = I18nUtil.getMessage(languageKey, Locale.SIMPLIFIED_CHINESE);
-        String i18noverModCapsUS = I18nUtil.getMessage(languageKey, Locale.US);
-
-        String CN = String.format(i18noverModCapsCN, format1, format2);
-        String US = String.format(i18noverModCapsUS, format1, format2);
+        String i18nOverModCapsCn = I18nUtil.getMessage(languageKey, Locale.SIMPLIFIED_CHINESE);
+        String i18nOverModCapsUs = I18nUtil.getMessage(languageKey, Locale.US);
+        String cn = String.format(i18nOverModCapsCn, format1, format2);
+        String us = String.format(i18nOverModCapsUs, format1, format2);
         // 构建 JSON 对象
         JSONObject reasonJson = new JSONObject();
-        reasonJson.put(CHINESE, CN);
-        reasonJson.put(ENGLISH, US);
+        reasonJson.put(I18nConstant.CHINESE, cn);
+        reasonJson.put(I18nConstant.ENGLISH, us);
         return reasonJson;
     }
 
@@ -94,31 +89,28 @@ public class JsonUtils {
      * @param format1     需要转换的参数
      * @return
      */
-    public static JSONObject getLanguageJsonObjectByReason(String reason,String languageKey, Object format1) {
-        String i18noverModCapsCN = I18nUtil.getMessage(languageKey, Locale.SIMPLIFIED_CHINESE);
-        String i18noverModCapsUS = I18nUtil.getMessage(languageKey, Locale.US);
-
-        String CN = "";
-        String US = "";
-
-        String reasonCN = "";
-        String reasonUS = "";
+    public static JSONObject getLanguageJsonObjectByReason(String reason, String languageKey, Object format1) {
+        String i18nOverModCapsCn = I18nUtil.getMessage(languageKey, Locale.SIMPLIFIED_CHINESE);
+        String i18nOverModCapsUs = I18nUtil.getMessage(languageKey, Locale.US);
+        String cn = "";
+        String us = "";
+        String reasonCn = "";
+        String reasonUs = "";
         //reason需要判断是否是json对象  如果是还需要转换一下  否则就不转换了 直接拼接
-        if (isJsonObject(reason)){
+        if (isJsonObject(reason)) {
             JSONObject jsonObject = JSON.parseObject(reason);
-            reasonCN = jsonObject.getString(CHINESE);
-            reasonUS = jsonObject.getString(ENGLISH);
-            CN = reasonCN + String.format(i18noverModCapsCN, format1);
-            US = reasonUS + String.format(i18noverModCapsUS, format1);
-        }else{
-            CN = reason + String.format(i18noverModCapsCN, format1);
-            US = reason + String.format(i18noverModCapsUS, format1);
+            reasonCn = jsonObject.getString(I18nConstant.CHINESE);
+            reasonUs = jsonObject.getString(I18nConstant.ENGLISH);
+            cn = reasonCn + String.format(i18nOverModCapsCn, format1);
+            us = reasonUs + String.format(i18nOverModCapsUs, format1);
+        } else {
+            cn = reason + String.format(i18nOverModCapsCn, format1);
+            us = reason + String.format(i18nOverModCapsUs, format1);
         }
-
         // 构建 JSON 对象
         JSONObject reasonJson = new JSONObject();
-        reasonJson.put(CHINESE, CN);
-        reasonJson.put(ENGLISH, US);
+        reasonJson.put(I18nConstant.CHINESE, cn);
+        reasonJson.put(I18nConstant.ENGLISH, us);
         return reasonJson;
     }
 
@@ -130,31 +122,28 @@ public class JsonUtils {
      * @param format2     需要转换的参数
      * @return
      */
-    public static JSONObject getLanguageJsonObjectByReason(String reason,String languageKey, Object format1, Object format2) {
-        String i18noverModCapsCN = I18nUtil.getMessage(languageKey, Locale.SIMPLIFIED_CHINESE);
-        String i18noverModCapsUS = I18nUtil.getMessage(languageKey, Locale.US);
-
-        String CN = "";
-        String US = "";
-
-        String reasonCN = "";
-        String reasonUS = "";
+    public static JSONObject getLanguageJsonObjectByReason(String reason, String languageKey, Object format1, Object format2) {
+        String i18nOverModCapsCn = I18nUtil.getMessage(languageKey, Locale.SIMPLIFIED_CHINESE);
+        String i18nOverModCapsUs = I18nUtil.getMessage(languageKey, Locale.US);
+        String cn = "";
+        String us = "";
+        String reasonCn = "";
+        String reasonUs = "";
         //reason需要判断是否是json对象  如果是还需要转换一下  否则就不转换了 直接拼接
-        if (isJsonObject(reason)){
+        if (isJsonObject(reason)) {
             JSONObject jsonObject = JSON.parseObject(reason);
-            reasonCN = jsonObject.getString(CHINESE);
-            reasonUS = jsonObject.getString(ENGLISH);
-            CN = reasonCN + String.format(i18noverModCapsCN, format1, format2);
-            US = reasonUS + String.format(i18noverModCapsUS, format1, format2);
-        }else{
-            CN = reason + String.format(i18noverModCapsCN, format1, format2);
-            US = reason + String.format(i18noverModCapsUS, format1, format2);
+            reasonCn = jsonObject.getString(I18nConstant.CHINESE);
+            reasonUs = jsonObject.getString(I18nConstant.ENGLISH);
+            cn = reasonCn + String.format(i18nOverModCapsCn, format1, format2);
+            us = reasonUs + String.format(i18nOverModCapsUs, format1, format2);
+        } else {
+            cn = reason + String.format(i18nOverModCapsCn, format1, format2);
+            us = reason + String.format(i18nOverModCapsUs, format1, format2);
         }
-
         // 构建 JSON 对象
         JSONObject reasonJson = new JSONObject();
-        reasonJson.put(CHINESE, CN);
-        reasonJson.put(ENGLISH, US);
+        reasonJson.put(I18nConstant.CHINESE, cn);
+        reasonJson.put(I18nConstant.ENGLISH, us);
         return reasonJson;
     }
 
@@ -165,7 +154,7 @@ public class JsonUtils {
      * @return 如果是有效的 JSON 对象，返回 true；否则返回 false
      */
     public static boolean isJsonObject(String str) {
-        if(str == null || str.isEmpty()){
+        if (str == null || str.isEmpty()) {
             return false;
         }
         try {
@@ -177,19 +166,23 @@ public class JsonUtils {
     }
 
     /**
-     * 解析json备注信息列表
+     * 解析json字段信息列表
+     *
+     * @param list   数据集合
+     * @param locale 语言
+     * @param fields 字段
      */
     public static void parseJsonRemarkList(List<? extends BaseEntity> list, String locale, String... fields) {
         if (CollectionUtils.isEmpty(list) || StringUtils.isEmpty(locale) || fields == null || fields.length == 0) {
             return;
         }
-
         for (BaseEntity item : list) {
             for (String field : fields) {
                 String fieldJson = (String) item.getFieldValueByFieldName(field);
-                if (StringUtils.isNotBlank(fieldJson)) {
-                    item.setFieldValueByFieldName(field, parseJsonRemark(fieldJson, locale));
+                if (StringUtils.isBlank(fieldJson)) {
+                    continue;
                 }
+                item.setFieldValueByFieldName(field, parseJsonRemark(fieldJson, locale));
             }
         }
     }
@@ -206,38 +199,71 @@ public class JsonUtils {
         if (StringUtils.isBlank(json) || StringUtils.isBlank(locale)) {
             return json;
         }
-
         StringBuilder builder = new StringBuilder();
-
         try {
+            //格式：[{"en_US":"...","zh_CN":"..."},{"en_US":"...","zh_CN":"..."}...]
             if (json.startsWith("[")) {
-                // 格式：[{"en_US":"...","zh_CN":"..."},{"en_US":"...","zh_CN":"..."}...]
-                JSONArray jsonArray = JSON.parseArray(json);
-                for (Object item : jsonArray) {
-                    JSONObject jsonItem = (JSONObject) item;
-                    builder.append(jsonItem.get(locale));
-                    builder.append(",");
-                }
-
-            } else if (json.startsWith("{")) {
-                // 格式：{"en_US":"...","zh_CN":"..."},{"en_US":"...","zh_CN":"..."}...
-                int index;
-                while ((index = json.indexOf("},{")) >= 0) {
-                    String tempJson = json.substring(0, index + 1);
-                    builder.append(JSON.parseObject(tempJson).get(locale));
-                    builder.append(",");
-                    json = json.substring(index + 2);
-                }
-                builder.append(JSON.parseObject(json).get(locale));
-                builder.append(",");
+                analysisStandardArrayJson(builder, json, locale);
+                return builder.length() > 0 ? builder.substring(0, builder.length() - 1) : json;
             }
-
+            //格式：{"en_US":"...","zh_CN":"..."},{"en_US":"...","zh_CN":"..."}...
+            if (json.startsWith("{")) {
+                analysisNoStandardArrayJson(builder, json, locale);
+                return builder.length() > 0 ? builder.substring(0, builder.length() - 1) : json;
+            }
             return builder.length() > 0 ? builder.substring(0, builder.length() - 1) : json;
         } catch (Exception e) {
             log.error("解析json备注信息失败", e);
         }
-
         return json;
     }
 
+    /**
+     * 解析标准数组格式的json
+     * 格式：[{"en_US":"...","zh_CN":"..."},{"en_US":"...","zh_CN":"..."}...]
+     *
+     * @param builder 数据存储
+     * @param json    json格式数据
+     * @param locale  语言
+     */
+    private static void analysisStandardArrayJson(StringBuilder builder, String json, String locale) {
+        if (StringUtils.isBlank(json)) {
+            return;
+        }
+        if (!json.startsWith("[")) {
+            return;
+        }
+        JSONArray jsonArray = JSON.parseArray(json);
+        for (Object item : jsonArray) {
+            JSONObject jsonItem = (JSONObject) item;
+            builder.append(jsonItem.get(locale));
+            builder.append(",");
+        }
+    }
+
+    /**
+     * 解析标非准数组格式的json
+     * 格式：{"en_US":"...","zh_CN":"..."},{"en_US":"...","zh_CN":"..."}...
+     *
+     * @param builder 数据存储
+     * @param json    json格式数据
+     * @param locale  语言
+     */
+    private static void analysisNoStandardArrayJson(StringBuilder builder, String json, String locale) {
+        if (StringUtils.isBlank(json)) {
+            return;
+        }
+        if (!json.startsWith("{")) {
+            return;
+        }
+        int index;
+        while ((index = json.indexOf("},{")) >= 0) {
+            String tempJson = json.substring(0, index + 1);
+            builder.append(JSON.parseObject(tempJson).get(locale));
+            builder.append(",");
+            json = json.substring(index + 2);
+        }
+        builder.append(JSON.parseObject(json).get(locale));
+        builder.append(",");
+    }
 }
--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/factory/controller/ProductionMonthPlanInitController.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/factory/controller/ProductionMonthPlanInitController.java
@@ -21,7 +21,7 @@ import java.util.List;
 /**
  * Copyright (c) 2022, All rights reserved。
  * 文件名称：ProductionMonthPlanInitController.java
- * 描    述：分厂月生产计划排产过程-计划初始化 控制层类：....
+ * 描    述：工厂月计划初始化 控制层类：....
  *
  * @author zlt
  * @version 1.0
@@ -30,10 +30,10 @@ import java.util.List;
  * 修改时间：...
  * 修 改 人：zlt
  * 修改内容：...
- * @date 2025-03-17
+ * @date 20251205
  */
 @Slf4j
-@Api(tags = "分厂月生产计划排产过程-计划初始化")
+@Api(tags = "工厂月计划初始化业务-服务类")
 @RestController
 @RequestMapping("/productionMonthPlanInit")
 @RequiredArgsConstructor
@@ -50,7 +50,7 @@ public class ProductionMonthPlanInitController extends BusiController<Production
     public TableDataInfo list(@RequestBody ProductionMonthPlanInit queryVO) {
         try {
             startPage();
-            List<ProductionMonthPlanInit> list = productionMonthPlanInitService.selectList(queryVO);
+            List<ProductionMonthPlanInit> list = productionMonthPlanInitService.getDataList(queryVO);
             return getDataTable(list);
         } finally {
             PageUtils.clearPage();
@@ -58,72 +58,19 @@ public class ProductionMonthPlanInitController extends BusiController<Production
     }
 
     /**
-     //  * 保存
-     //  */
-    // @Log(title = "ui.data.column.productionMonthPlanInit.modelName", businessType = BusinessType.INSERT_OR_UPDATE)
-    // @RequiresPermissions("monthplan:productionMonthPlanInit:save")
-    // @ApiOperation("保存")
-    // @PostMapping("/save")
-    // @Override
-    // public AjaxResult save(@RequestBody ProductionMonthPlanInit billVO) {
-    //     return super.save(billVO);
-    // }
-    //
-    // /**
-    //  * 删除
-    //  */
-    // @Log(title = "ui.data.column.productionMonthPlanInit.modelName", businessType = BusinessType.DELETE)
-    // @RequiresPermissions("monthplan:productionMonthPlanInit:remove")
-    // @ApiOperation("删除")
-    // @DeleteMapping("/remove")
-    // @Override
-    // public AjaxResult removeByIds(@RequestBody List<Long> ids) {
-    //     return super.removeByIds(ids);
-    // }
-
-
-    // /**
-    //  * 获取分厂月生产计划排产过程-计划初始化详细信息
-    //  */
-    // @RequiresPermissions("monthplan:productionMonthPlanInit:query")
-    // @ApiOperation("获取详细信息")
-    // @GetMapping(value = "/{billId}")
-    // @Override
-    // public ProductionMonthPlanInit getInfo(@PathVariable("billId") Long billId) {
-    //     return super.getInfo(billId);
-    // }
-
-
-    // /**
-    //  * 根据集合导入分厂月生产计划排产过程-计划初始化数据
-    //  *
-    //  * @param importContext 导入上下文
-    //  * @param updateSupport 已存在记录是否更新
-    //  * @return 结果
-    //  */
-    // @RequiresPermissions("monthplan:productionMonthPlanInit:import")
-    // @Log(title = "ui.data.column.productionMonthPlanInit.modelName", businessType = BusinessType.IMPORT)
-    // @ApiOperation("导入数据")
-    // @PostMapping("/importData")
-    // @Override
-    // public AjaxResult importData(@RequestBody ImportContext importContext, @RequestParam("updateSupport") boolean updateSupport) throws Exception {
-    //     return super.importData(importContext, updateSupport);
-    // }
-
-    /**
      * 导出列表
      */
     @RequiresPermissions("monthplan:productionMonthPlanInit:export")
-    @Log(title = "分厂月生产计划排产过程-计划初始化", businessType = BusinessType.EXPORT)
-    @ApiOperation("导入数据")
+    @Log(title = "工厂月计划初始化", businessType = BusinessType.EXPORT)
+    @ApiOperation("导出数据")
     @PostMapping("/exportData/{fileName}")
-    public byte[] exportData(@RequestBody ProductionMonthPlanInit queryVO, @PathVariable("fileName") String fileName,
+    public byte[] exportData(@RequestBody ProductionMonthPlanInit condition, @PathVariable("fileName") String fileName,
                              HttpServletResponse response) throws IOException {
-        return super.commonExport(queryVO, fileName, response);
+        return super.commonExport(condition, fileName, response);
     }
 
     @Override
     protected List<ProductionMonthPlanInit> listExportData(ProductionMonthPlanInit obj) {
-        return productionMonthPlanInitService.selectList(obj);
+        return productionMonthPlanInitService.getDataList(obj);
     }
 }
