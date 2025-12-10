7e599a8f9109b2f45b6beeac0e1c226ceafa37f9
kf_nick <kf_nick@qq.com>
2025-12-10 19:15:51 +0800

    原材料需求模块-原材料比例配置 提交 Nick+

--- Aps-BootUI/src/main/java/com/zlt/aps/controller/raw/RawMaterialOutboundRecordUIController.java
+++ Aps-BootUI/src/main/java/com/zlt/aps/controller/raw/RawMaterialOutboundRecordUIController.java
@@ -173,7 +173,7 @@ public class RawMaterialOutboundRecordUIController extends BaseUIController<RawM
         context.setProcedureCode(this.getProcedureCode());
         context.setOriFileName(file.getOriginalFilename());
         context.setFileBytes(data);
-        AjaxResult ajaxResult = iRawMaterialOutboundRecordService.importData(context,false);
+        AjaxResult ajaxResult = iRawMaterialOutboundRecordService.importData(context,true);
         return ajaxResult;
     }
 }
