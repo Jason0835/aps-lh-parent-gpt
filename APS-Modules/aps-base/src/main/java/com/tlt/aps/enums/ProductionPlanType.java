45d5e685a4a1eb9c006f1506295c6ce8bc830b60
Abdul.Wu <wushuigen312@163.com>
2025-12-11 18:00:07 +0800

    排产计划类型枚举定义类

---
+++ APS-Modules/aps-base/src/main/java/com/tlt/aps/enums/ProductionPlanType.java
@@ -0,0 +1,35 @@
+package com.tlt.aps.enums;
+
+import lombok.Getter;
+
+/**
+ * 排产计划类型
+ * 01 正常 02 订单预测 03 实单模拟
+ *
+ * @author zlt
+ * @since 20251211
+ */
+@Getter
+public enum ProductionPlanType {
+    /**
+     * 01 正常
+     */
+    normal("01", "正常"),
+    /**
+     * 02 订单预测
+     */
+    prediction("02", "订单预测"),
+    /**
+     * 03 实单模拟
+     */
+    simulate("03", "实单模拟");
+
+    private String planType;
+
+    private String desc;
+
+    ProductionPlanType(String planType, String desc) {
+        this.planType = planType;
+        this.desc = desc;
+    }
+}
