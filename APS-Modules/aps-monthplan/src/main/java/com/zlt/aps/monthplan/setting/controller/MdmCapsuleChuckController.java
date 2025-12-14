d4726489f2bc352ab9639af0d4c1ee962017e242
wengpc <wengpc@zlt.com>
2025-12-14 12:22:25 +0800

    feat:胶囊卡盘台账

---
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/setting/controller/MdmCapsuleChuckController.java
@@ -0,0 +1,172 @@
+package com.zlt.aps.monthplan.setting.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.ruoyi.common.security.annotation.RequiresPermissions;
+import com.zlt.aps.maindata.mapper.MdmCapsuleChuckEntityMapper;
+import com.zlt.aps.maindata.service.IMdmCapsuleChuckService;
+import com.zlt.aps.monthplan.api.domain.entity.MdmCapsuleChuck;
+import com.zlt.common.utils.PubUtil;
+import com.ruoyi.api.gateway.system.domain.vo.ImportContext;
+import lombok.extern.slf4j.Slf4j;
+import com.ruoyi.common.core.web.domain.AjaxResult;
+import com.ruoyi.common.i18n.utils.I18nUtil;
+import com.ruoyi.common.log.annotation.Log;
+import com.ruoyi.common.log.enums.BusinessType;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+
+import com.ruoyi.common.core.web.page.TableDataInfo;
+
+import com.zlt.bill.common.controller.AbstractDocBizController;
+import com.zlt.bill.common.service.IDocService ;
+
+/**
+* Copyright (c) 2022, All rights reserved。
+* 文件名称：MdmCapsuleChuckController.java
+* 描    述：胶囊卡盘台账 控制层类：....
+*@author zlt
+*@date 2025-12-12
+*@version 1.0
+*
+ *  修改记录：
+*     修改时间：...
+*     修 改 人：zlt
+*     修改内容：...
+*/
+@Slf4j
+@Api(tags = "胶囊卡盘台账")
+@RestController
+@RequestMapping("/mdmCapsuleChuck")
+public class MdmCapsuleChuckController extends AbstractDocBizController<MdmCapsuleChuck> {
+
+    @Autowired
+    private IMdmCapsuleChuckService mdmCapsuleChuckService;
+
+    @Autowired
+    private MdmCapsuleChuckEntityMapper entityMapper;
+
+    /**
+     * 查询胶囊卡盘台账列表
+     */
+    @RequiresPermissions( "monthplan:mdmCapsuleChuck:list")
+    @ApiOperation("查询列表")
+    @PostMapping("/list")
+    @Override
+    public TableDataInfo list(@RequestBody MdmCapsuleChuck queryVO) {
+        return super.list(queryVO);
+    }
+
+    @Override
+    protected String getOrderBy() {
+        return "create_time desc";
+    }
+
+    /**
+     * 保存
+     */
+    @Log(title = "ui.data.column.mdmCapsuleChuck.modelName", businessType = BusinessType.INSERT_OR_UPDATE)
+    @RequiresPermissions( "monthplan:mdmCapsuleChuck:save")
+    @ApiOperation("保存")
+    @PostMapping("/save")
+    @Override
+    public AjaxResult save(@RequestBody MdmCapsuleChuck billVO){
+        return super.save(billVO);
+    }
+
+    /**
+     * 删除
+     */
+    @Log(title = "ui.data.column.mdmCapsuleChuck.modelName", businessType = BusinessType.DELETE)
+    @RequiresPermissions( "monthplan:mdmCapsuleChuck:remove")
+    @ApiOperation("删除")
+    @DeleteMapping("/remove")
+    @Override
+    public AjaxResult removeByIds(@RequestBody List<Long> ids){
+        return super.removeByIds(ids);
+    }
+
+
+    /**
+     * 获取胶囊卡盘台账详细信息
+     */
+    @RequiresPermissions( "monthplan:mdmCapsuleChuck:query")
+    @ApiOperation("获取详细信息")
+    @GetMapping(value = "/{billId}")
+    @Override
+    public MdmCapsuleChuck getInfo(@PathVariable("billId") Long billId) {
+        return super.getInfo(billId);
+    }
+
+
+    /**
+     * 根据集合导入胶囊卡盘台账数据
+     * @param importContext 导入上下文
+     * @param updateSupport 已存在记录是否更新
+     * @return 结果
+     */
+    @RequiresPermissions( "monthplan:mdmCapsuleChuck:import")
+    @Log(title = "ui.data.column.mdmCapsuleChuck.modelName", businessType = BusinessType.IMPORT)
+    @ApiOperation("导入数据")
+    @PostMapping("/importData")
+    @Override
+    public AjaxResult importData(@RequestBody ImportContext importContext, @RequestParam("updateSupport") boolean updateSupport) throws Exception {
+        return super.importData(importContext,updateSupport);
+    }
+
+    /**
+     * 导出列表
+     */
+    @RequiresPermissions( "monthplan:mdmCapsuleChuck:export")
+    @Log(title = "胶囊卡盘台账", businessType = BusinessType.EXPORT)
+    @ApiOperation("导出数据")
+    @PostMapping("/exportData/{fileName}")
+    @Override
+    public byte[] exportData(@RequestBody MdmCapsuleChuck queryVO, @PathVariable("fileName") String fileName,
+                             HttpServletResponse response) throws IOException {
+        return super.exportData(queryVO, fileName, response);
+    }
+
+    @Override
+    protected List<MdmCapsuleChuck> listExportData(MdmCapsuleChuck obj) {
+        QueryWrapper<MdmCapsuleChuck> wrapper = new QueryWrapper<>();
+        this.builderCondition(wrapper, obj);
+        return entityMapper.selectList(wrapper);
+    }
+
+    @Override
+    protected IDocService getDocService(){
+        return mdmCapsuleChuckService;
+    }
+
+    /**
+     * 条件拼接
+     *
+     * @param queryWrapper
+     * @param queryVO
+     */
+    @Override
+    protected void builderCondition(QueryWrapper<MdmCapsuleChuck> queryWrapper, MdmCapsuleChuck queryVO) {
+        queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("factoryCode")), "FACTORY_CODE", queryVO.getFieldValueByFieldName("factoryCode"));
+        queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("proSize")), "PRO_SIZE", queryVO.getFieldValueByFieldName("proSize"));
+        queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("specifications")), "SPECIFICATIONS", queryVO.getFieldValueByFieldName("specifications"));
+        queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("internalQty")), "INTERNAL_QTY", queryVO.getFieldValueByFieldName("internalQty"));
+        queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("newChuckQty")), "NEW_CHUCK_QTY", queryVO.getFieldValueByFieldName("newChuckQty"));
+    }
+
+
+    @Override
+    protected String getTypeCode(){
+        return "MDM0137";
+    }
+
+
+}
---
+++ Aps-Api/aps-monthplan-api/src/main/java/com/zlt/aps/monthplan/api/domain/entity/MdmCapsuleChuck.java
@@ -0,0 +1,64 @@
+package com.zlt.aps.monthplan.api.domain.entity;
+
+import com.ruoyi.common.core.web.domain.BaseEntity;
+import lombok.Data;
+import com.ruoyi.common.core.annotation.Excel;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.zlt.common.domain.CommonBusiEntity;
+
+/**
+ * Copyright (c) 2022, All rights reserved。
+ * 文件名称：MdmCapsuleChuck.java
+ * 描    述：胶囊卡盘台账对象 t_mdm_capsule_chuck
+ *@author zlt
+ *@date 2025-12-12
+ *@version 1.0
+ *
+ *  修改记录：
+ *     修改时间：...
+ *     修 改 人：zlt
+ *     修改内容：...
+ */
+
+@ApiModel(value = "胶囊卡盘台账对象", description = "胶囊卡盘台账对象 ")
+@Data
+@TableName(value = "T_MDM_CAPSULE_CHUCK")
+public class MdmCapsuleChuck extends BaseEntity {
+
+    private static final long serialVersionUID = 1L;
+
+     /** 工厂编号 */
+    @Excel(name = "ui.data.column.mdmCapsuleChuck.factoryCode")
+    @ApiModelProperty(value = "工厂编号", name = "factoryCode")
+    @TableField(value = "FACTORY_CODE")
+    private String factoryCode;
+
+    /** TBR卡盘英寸 多个以,分隔 */
+    @Excel(name = "ui.data.column.mdmCapsuleChuck.proSize")
+    @ApiModelProperty(value = "TBR卡盘英寸 多个以,分隔", name = "proSize")
+    @TableField(value = "PRO_SIZE")
+    private String proSize;
+
+    /** TBR卡盘 多个以,分隔 */
+    @Excel(name = "ui.data.column.mdmCapsuleChuck.specifications")
+    @ApiModelProperty(value = "TBR卡盘 多个以,分隔", name = "specifications")
+    @TableField(value = "SPECIFICATIONS")
+    private String specifications;
+
+    /** 国内转移 */
+    @Excel(name = "ui.data.column.mdmCapsuleChuck.internalQty")
+    @ApiModelProperty(value = "国内转移", name = "internalQty")
+    @TableField(value = "INTERNAL_QTY")
+    private Integer internalQty;
+
+    /** JINYU新卡盘 */
+    @Excel(name = "ui.data.column.mdmCapsuleChuck.newChuckQty")
+    @ApiModelProperty(value = "JINYU新卡盘", name = "newChuckQty")
+    @TableField(value = "NEW_CHUCK_QTY")
+    private Integer newChuckQty;
+
+
+}
