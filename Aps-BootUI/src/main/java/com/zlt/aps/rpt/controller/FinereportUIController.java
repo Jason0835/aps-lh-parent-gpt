8998eebc81538c32f57fe7cb9058c3a88aadaace
kf_hak <KF_Hak@zlt.com>
2025-12-19 11:13:03 +0800

    库存库龄分析报表接口调整成post接口

--- Aps-BootUI/src/main/java/com/zlt/aps/rpt/controller/FinereportUIController.java
+++ Aps-BootUI/src/main/java/com/zlt/aps/rpt/controller/FinereportUIController.java
@@ -3,7 +3,7 @@ package com.zlt.aps.rpt.controller;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,7 +23,7 @@ public class FinereportUIController extends BaseController {
 
 	@ApiOperation("根据条件查询外协规格管理列表")
 	@RequiresPermissions("rpt:inventoryAgeAnalysis")
-	@GetMapping("/inventoryAgeAnalysis")
+	@PostMapping("/inventoryAgeAnalysis")
 	@ResponseBody
 	public AjaxResult inventoryAgeAnalysis() {
 		return iFinereportService.inventoryAgeAnalysis();
