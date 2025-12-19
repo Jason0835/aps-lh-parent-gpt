402cdbcfea9f3e9095e13bdf8c0785886272cf88
kf_steve <KF_Steve@zlt.com>
2025-12-19 15:03:04 +0800

    月均销量列表展示

--- Aps-BootUI/src/main/java/com/zlt/aps/controller/maindata/MpMonthlySaleQtyUIController.java
+++ Aps-BootUI/src/main/java/com/zlt/aps/controller/maindata/MpMonthlySaleQtyUIController.java
@@ -199,6 +199,18 @@ public class MpMonthlySaleQtyUIController extends BaseUIController<MpMonthlySale
         return ajaxResult;
     }
 
+
+    /**
+     * 获取表头展示列表
+     *
+     * @return 结果
+     */
+    @PostMapping("/getShowTableTitleList")
+    @ApiOperation("获取表头展示列表")
+    public AjaxResult getShowTableTitleList(MpMonthlySaleQty queryVO) {
+        return iMpMonthlySaleQtyService.getShowTableTitleList(queryVO);
+    }
+
     /**
      * 生成月均销量
      *
