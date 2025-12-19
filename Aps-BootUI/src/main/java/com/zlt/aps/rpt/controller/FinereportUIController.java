0949a0f06dd36293c1452c12e70f8ee9f36080f1
kf_hak <KF_Hak@zlt.com>
2025-12-19 11:06:33 +0800

    增加库存库龄报表前端访问接口

---
+++ Aps-BootUI/src/main/java/com/zlt/aps/rpt/controller/FinereportUIController.java
@@ -0,0 +1,31 @@
+package com.zlt.aps.rpt.controller;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ruoyi.common.core.web.controller.BaseController;
+import com.ruoyi.common.core.web.domain.AjaxResult;
+import com.zlt.aps.itf.finereport.IFinereportService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = { "帆软报表控制器" })
+@Controller
+@RequestMapping("/finereport")
+public class FinereportUIController extends BaseController {
+	@Autowired
+	private IFinereportService iFinereportService;
+
+	@ApiOperation("根据条件查询外协规格管理列表")
+	@RequiresPermissions("rpt:inventoryAgeAnalysis")
+	@GetMapping("/inventoryAgeAnalysis")
+	@ResponseBody
+	public AjaxResult inventoryAgeAnalysis() {
+		return iFinereportService.inventoryAgeAnalysis();
+	}
+}
