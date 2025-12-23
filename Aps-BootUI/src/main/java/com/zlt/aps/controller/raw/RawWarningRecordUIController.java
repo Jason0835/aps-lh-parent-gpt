0547799b3e861599e23a64d3dc7ecd5ea7ec5e9c
kf_nick <kf_nick@qq.com>
2025-12-23 19:05:49 +0800

    18875 原材料预警记录------>在执行用量偏差预警的时候没有输入周次情况下，接口返回提示服务器错误

--- Aps-BootUI/src/main/java/com/zlt/aps/controller/raw/RawWarningRecordUIController.java
+++ Aps-BootUI/src/main/java/com/zlt/aps/controller/raw/RawWarningRecordUIController.java
@@ -106,6 +106,18 @@ public class RawWarningRecordUIController extends BaseUIController<RawWarningRec
                                           @RequestParam("year") Integer year,
                                           @RequestParam("week") Integer week,
                                           @RequestParam("month") Integer month) {
+        if (week == null) {
+            return AjaxResult.error(I18nUtil.getMessage("ui.data.column.rawWarningRecord.modelName.week"));
+        }
+
+        if (month == null) {
+            return AjaxResult.error(I18nUtil.getMessage("ui.data.column.rawWarningRecord.modelName.month"));
+        }
+
+        if (year == null) {
+            return AjaxResult.error(I18nUtil.getMessage("ui.data.column.rawWarningRecord.modelName.year"));
+        }
+
         return iRawWarningRecordService.executeUsageWarning(factoryCode, year, week, month);
     }
 
