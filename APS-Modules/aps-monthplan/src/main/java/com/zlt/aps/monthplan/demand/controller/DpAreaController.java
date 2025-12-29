fbd9b952acf623d28d18d7fd1fef5bd33a6ac572
kf_steve <KF_Steve@zlt.com>
2025-12-29 11:45:19 +0800

    区域选择

---
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/demand/controller/DpAreaController.java
@@ -0,0 +1,170 @@
+package com.zlt.aps.monthplan.demand.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.ruoyi.api.gateway.system.domain.vo.ImportContext;
+import com.ruoyi.common.core.web.domain.AjaxResult;
+import com.ruoyi.common.core.web.page.TableDataInfo;
+import com.ruoyi.common.log.annotation.Log;
+import com.ruoyi.common.log.enums.BusinessType;
+import com.ruoyi.common.security.annotation.RequiresPermissions;
+import com.zlt.aps.monthplan.api.domain.entity.DpArea;
+import com.zlt.aps.monthplan.demand.mapper.DpAreaEntityMapper;
+import com.zlt.aps.monthplan.demand.service.IDpAreaService;
+import com.zlt.bill.common.controller.AbstractDocBizController;
+import com.zlt.bill.common.service.IDocService;
+import com.zlt.common.utils.PubUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Copyright (c) 2022, All rights reserved。
+ * 文件名称：DpAreaController.java
+ * 描    述：区域 控制层类：....
+ *
+ * @author zlt
+ * @version 1.0
+ * <p>
+ * 修改记录：
+ * 修改时间：...
+ * 修 改 人：zlt
+ * 修改内容：...
+ * @date 2025-12-29
+ */
+@Slf4j
+@Api(tags = "区域")
+@RestController
+@RequestMapping("/dpArea")
+public class DpAreaController extends AbstractDocBizController<DpArea> {
+
+    @Autowired
+    private IDpAreaService dpAreaService;
+
+    @Autowired
+    private DpAreaEntityMapper entityMapper;
+
+    /**
+     * 查询区域列表
+     */
+    @RequiresPermissions("monthplan:dpArea:list")
+    @ApiOperation("查询列表")
+    @PostMapping("/list")
+    @Override
+    public TableDataInfo list(@RequestBody DpArea queryVO) {
+        return super.list(queryVO);
+    }
+
+    @Override
+    protected String getOrderBy() {
+        return "create_time desc";
+    }
+
+    /**
+     * 保存
+     */
+    @Log(title = "ui.data.column.dpArea.modelName", businessType = BusinessType.INSERT_OR_UPDATE)
+    @RequiresPermissions("monthplan:dpArea:save")
+    @ApiOperation("保存")
+    @PostMapping("/save")
+    @Override
+    public AjaxResult save(@RequestBody DpArea billVO) {
+        return super.save(billVO);
+    }
+
+    /**
+     * 删除
+     */
+    @Log(title = "ui.data.column.dpArea.modelName", businessType = BusinessType.DELETE)
+    @RequiresPermissions("monthplan:dpArea:remove")
+    @ApiOperation("删除")
+    @DeleteMapping("/remove")
+    @Override
+    public AjaxResult removeByIds(@RequestBody List<Long> ids) {
+        return super.removeByIds(ids);
+    }
+
+
+    /**
+     * 获取区域详细信息
+     */
+    @RequiresPermissions("monthplan:dpArea:query")
+    @ApiOperation("获取详细信息")
+    @GetMapping(value = "/{billId}")
+    @Override
+    public DpArea getInfo(@PathVariable("billId") Long billId) {
+        return super.getInfo(billId);
+    }
+
+
+    /**
+     * 根据集合导入区域数据
+     *
+     * @param importContext 导入上下文
+     * @param updateSupport 已存在记录是否更新
+     * @return 结果
+     */
+    @RequiresPermissions("monthplan:dpArea:import")
+    @Log(title = "ui.data.column.dpArea.modelName", businessType = BusinessType.IMPORT)
+    @ApiOperation("导入数据")
+    @PostMapping("/importData")
+    @Override
+    public AjaxResult importData(@RequestBody ImportContext importContext, @RequestParam("updateSupport") boolean updateSupport) throws Exception {
+        return super.importData(importContext, updateSupport);
+    }
+
+    /**
+     * 导出列表
+     */
+    @RequiresPermissions("monthplan:dpArea:export")
+    @Log(title = "区域", businessType = BusinessType.EXPORT)
+    @ApiOperation("导入数据")
+    @PostMapping("/exportData/{fileName}")
+    @Override
+    public byte[] exportData(@RequestBody DpArea queryVO, @PathVariable("fileName") String fileName,
+                             HttpServletResponse response) throws IOException {
+        return super.exportData(queryVO, fileName, response);
+    }
+
+    @Override
+    protected List<DpArea> listExportData(DpArea obj) {
+        QueryWrapper<DpArea> wrapper = new QueryWrapper<>();
+        this.builderCondition(wrapper, obj);
+        return entityMapper.selectList(wrapper);
+    }
+
+    @Override
+    protected IDocService getDocService() {
+        return dpAreaService;
+    }
+
+    /**
+     * 条件拼接
+     *
+     * @param queryWrapper
+     * @param queryVO
+     */
+    @Override
+    protected void builderCondition(QueryWrapper<DpArea> queryWrapper, DpArea queryVO) {
+        queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("areaCode")), "AREA_CODE", queryVO.getFieldValueByFieldName("areaCode"));
+        queryWrapper.like(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("areaName")), "AREA_NAME", queryVO.getFieldValueByFieldName("areaName"));
+        queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("areaType")), "AREA_TYPE", queryVO.getFieldValueByFieldName("areaType"));
+        queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("parentCode")), "PARENT_CODE", queryVO.getFieldValueByFieldName("parentCode"));
+        queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("crmCode")), "CRM_CODE", queryVO.getFieldValueByFieldName("crmCode"));
+        queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("isEudr")), "IS_EUDR", queryVO.getFieldValueByFieldName("isEudr"));
+        queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("ncCode")), "NC_CODE", queryVO.getFieldValueByFieldName("ncCode"));
+    }
+
+
+    @Override
+    protected String getTypeCode() {
+        return "DP0208";
+    }
+
+
+}
---
+++ Aps-Api/aps-monthplan-api/src/main/java/com/zlt/aps/monthplan/api/domain/entity/DpArea.java
@@ -0,0 +1,89 @@
+package com.zlt.aps.monthplan.api.domain.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.ruoyi.common.core.annotation.Excel;
+import com.ruoyi.common.core.web.domain.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * Copyright (c) 2022, All rights reserved。
+ * 文件名称：DpArea.java
+ * 描    述：区域对象 t_dp_area
+ *
+ * @author zlt
+ * @version 1.0
+ * <p>
+ * 修改记录：
+ * 修改时间：...
+ * 修 改 人：zlt
+ * 修改内容：...
+ * @date 2025-12-29
+ */
+@ApiModel(value = "区域对象", description = "区域对象 ")
+@Data
+@TableName(value = "T_DP_AREA")
+public class DpArea extends BaseEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 编码
+     */
+    @Excel(name = "ui.data.column.dpArea.areaCode")
+    @ApiModelProperty(value = "编码", name = "areaCode")
+    @TableField(value = "AREA_CODE")
+    private String areaCode;
+
+    /**
+     * 名称
+     */
+    @Excel(name = "ui.data.column.dpArea.areaName")
+    @ApiModelProperty(value = "名称", name = "areaName")
+    @TableField(value = "AREA_NAME")
+    private String areaName;
+
+    /**
+     * 类型01大洲，02国家/地区,03省/州，04市，05区/县
+     */
+    @Excel(name = "ui.data.column.dpArea.areaType")
+    @ApiModelProperty(value = "类型01大洲，02国家/地区,03省/州，04市，05区/县", name = "areaType")
+    @TableField(value = "AREA_TYPE")
+    private String areaType;
+
+    /**
+     * 上级编码
+     */
+    @Excel(name = "ui.data.column.dpArea.parentCode")
+    @ApiModelProperty(value = "上级编码", name = "parentCode")
+    @TableField(value = "PARENT_CODE")
+    private String parentCode;
+
+    /**
+     * CRM的编码
+     */
+    @Excel(name = "ui.data.column.dpArea.crmCode")
+    @ApiModelProperty(value = "CRM的编码", name = "crmCode")
+    @TableField(value = "CRM_CODE")
+    private String crmCode;
+
+    /**
+     * EUDR要求
+     */
+    @Excel(name = "ui.data.column.dpArea.isEudr")
+    @ApiModelProperty(value = "EUDR要求", name = "isEudr")
+    @TableField(value = "IS_EUDR")
+    private String isEudr;
+
+    /**
+     * NC编码
+     */
+    @Excel(name = "ui.data.column.dpArea.ncCode")
+    @ApiModelProperty(value = "NC编码", name = "ncCode")
+    @TableField(value = "NC_CODE")
+    private String ncCode;
+
+
+}
