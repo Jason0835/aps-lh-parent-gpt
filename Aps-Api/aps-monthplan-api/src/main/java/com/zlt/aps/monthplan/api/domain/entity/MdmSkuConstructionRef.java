584abbed978e3b858197640345b5950e59cee7ab
wengpc <wengpc@zlt.com>
2025-12-30 16:58:11 +0800

    19087 【SKU与施工关系】前端，导出和导入，去掉合模压力、成型法 19136 【 胶囊卡盘台账】新增 编辑 导入 业务唯一键“工厂加TBR卡盘(英寸) 加TBR卡盘(规格)” 19128 【SKU与示方书关系】列表问题

--- Aps-Api/aps-monthplan-api/src/main/java/com/zlt/aps/monthplan/api/domain/entity/MdmSkuConstructionRef.java
+++ Aps-Api/aps-monthplan-api/src/main/java/com/zlt/aps/monthplan/api/domain/entity/MdmSkuConstructionRef.java
@@ -35,7 +35,7 @@ public class MdmSkuConstructionRef extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
      /** 工厂编号 */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.factoryCode", dictType = "biz_factory_name")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.factoryCode", dictType = "biz_factory_name", sort = 1)
     @ApiModelProperty(value = "工厂编号", name = "factoryCode")
     @ImportExcelValidated(required = true)
     @TableField(value = "FACTORY_CODE")
@@ -43,95 +43,95 @@ public class MdmSkuConstructionRef extends BaseEntity {
 
     /** 物料编码 */
     @ImportExcelValidated(required = true)
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.materialCode")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.materialCode", sort = 15)
     @ApiModelProperty(value = "物料编码", name = "materialCode")
     @TableField(value = "MATERIAL_CODE")
     private String materialCode;
 
     /** MES物料编码 */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.mesMaterialCode")
+//    @Excel(name = "ui.data.column.mdmSkuConstructionRef.mesMaterialCode")
     @ApiModelProperty(value = "MES物料编码", name = "mesMaterialCode")
     @TableField(value = "MES_MATERIAL_CODE")
     private String mesMaterialCode;
 
     /** 规格代号 */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.specCode")
+//    @Excel(name = "ui.data.column.mdmSkuConstructionRef.specCode")
     @ApiModelProperty(value = "规格代号", name = "specCode")
     @TableField(value = "SPEC_CODE")
     private String specCode;
 
     /** 施工代号 */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.constructionCode")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.constructionCode", sort = 30)
     @ApiModelProperty(value = "施工代号", name = "constructionCode")
     @TableField(value = "CONSTRUCTION_CODE")
     private String constructionCode;
 
     /** 胎胚号 */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.embryoCode")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.embryoCode", sort = 45)
     @ApiModelProperty(value = "胎胚号", name = "embryoCode")
     @TableField(value = "EMBRYO_CODE")
     private String embryoCode;
 
     /** 生产版本 */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.productionVersion")
+//    @Excel(name = "ui.data.column.mdmSkuConstructionRef.productionVersion")
     @ApiModelProperty(value = "生产版本", name = "productionVersion")
     @TableField(value = "PRODUCTION_VERSION")
     private String productionVersion;
 
     /** 成型法 */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.mouldMethod", dictType = "molding_method")
+//    @Excel(name = "ui.data.column.mdmSkuConstructionRef.mouldMethod", dictType = "molding_method")
     @ApiModelProperty(value = "成型法", name = "mouldMethod")
     @TableField(value = "MOULD_METHOD")
     private String mouldMethod;
 
     /** BOM版本 */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.bomVersion")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.bomVersion", sort = 10)
     @ApiModelProperty(value = "BOM版本", name = "bomVersion")
     @TableField(value = "BOM_VERSION")
     private String bomVersion;
 
     /** 合模压力 */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.mouldClampingPressure")
+//    @Excel(name = "ui.data.column.mdmSkuConstructionRef.mouldClampingPressure")
     @ApiModelProperty(value = "合模压力", name = "mouldClampingPressure")
     @TableField(value = "MOULD_CLAMPING_PRESSURE")
     private BigDecimal mouldClampingPressure;
 
     /** 模具型腔 */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.mouldCavity")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.mouldCavity", sort = 20)
     @ApiModelProperty(value = "模具型腔", name = "mouldCavity")
     @TableField(value = "MOULD_CAVITY")
     private String mouldCavity;
 
     /** 是否零度材料 */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.isZeroRack", dictType = "biz_yes_no")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.isZeroRack", dictType = "biz_yes_no", sort = 25)
     @ApiModelProperty(value = "是否零度材料", name = "isZeroRack")
     @TableField(value = "IS_ZERO_RACK")
     private String isZeroRack;
 
     /** 夏季机械硫化时间(秒) */
     @ImportExcelValidated(digits = true)
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.curingTime")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.curingTime", sort = 35)
     @ApiModelProperty(value = "夏季机械硫化时间(秒)", name = "curingTime")
     @TableField(value = "CURING_TIME")
     private Integer curingTime;
 
     /** 夏季液压硫化时间(秒) */
     @ImportExcelValidated(digits = true)
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.hydraulicPressureCuringTime")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.hydraulicPressureCuringTime", sort = 50)
     @ApiModelProperty(value = "夏季液压硫化时间(秒)", name = "hydraulicPressureCuringTime")
     @TableField(value = "HY_PRESSURE_CURING_TIME")
     private Integer hydraulicPressureCuringTime;
 
     /** 冬季机械硫化时间(秒) */
     @ImportExcelValidated(digits = true)
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.curingTime2")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.curingTime2", sort = 40)
     @ApiModelProperty(value = "冬季机械硫化时间(秒)", name = "curingTime2")
     @TableField(value = "CURING_TIME2")
     private Integer curingTime2;
 
     /** 冬季液压硫化时间(秒) */
     @ImportExcelValidated(digits = true)
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.hydraulicPressureCuringTime2")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.hydraulicPressureCuringTime2", sort = 55)
     @ApiModelProperty(value = "冬季液压硫化时间(秒)", name = "hydraulicPressureCuringTime2")
     @TableField(value = "HY_PRESSURE_CURING_TIME2")
     private Integer hydraulicPressureCuringTime2;
@@ -139,7 +139,7 @@ public class MdmSkuConstructionRef extends BaseEntity {
     /**
      * 制造示方书号
      */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.embryoNo")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.embryoNo", sort = 60)
     @ApiModelProperty(value = "制造示方书号", name = "embryoNo")
     @TableField(value = "EMBRYO_NO")
     private String embryoNo;
@@ -147,7 +147,7 @@ public class MdmSkuConstructionRef extends BaseEntity {
     /**
      * 制造示方书类型
      */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.embryoType")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.embryoType", sort = 65)
     @ApiModelProperty(value = "制造示方书类型", name = "embryoType")
     @TableField(value = "EMBRYO_TYPE")
     private String embryoType;
@@ -157,7 +157,7 @@ public class MdmSkuConstructionRef extends BaseEntity {
      */
     @ImportExcelValidated(date = true)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.embryoReleaseDate", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.embryoReleaseDate", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss", sort = 70)
     @ApiModelProperty(value = "制造示方书发行时间", name = "embryoReleaseDate")
     @TableField(value = "EMBRYO_RELEASE_DATE")
     private Date embryoReleaseDate;
@@ -165,7 +165,7 @@ public class MdmSkuConstructionRef extends BaseEntity {
     /**
      * 文字示方书号
      */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.textNo")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.textNo", sort = 75)
     @ApiModelProperty(value = "文字示方书号", name = "textNo")
     @TableField(value = "TEXT_NO")
     private String textNo;
@@ -173,7 +173,7 @@ public class MdmSkuConstructionRef extends BaseEntity {
     /**
      * 文字示方书类型
      */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.textType")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.textType", sort = 80)
     @ApiModelProperty(value = "文字示方书类型", name = "textType")
     @TableField(value = "TEXT_TYPE")
     private String textType;
@@ -183,7 +183,7 @@ public class MdmSkuConstructionRef extends BaseEntity {
      */
     @ImportExcelValidated(date = true)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.textReleaseDate", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.textReleaseDate", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss", sort = 85)
     @ApiModelProperty(value = "文字示方书发行时间", name = "textReleaseDate")
     @TableField(value = "TEXT_RELEASE_DATE")
     private Date textReleaseDate;
@@ -191,7 +191,7 @@ public class MdmSkuConstructionRef extends BaseEntity {
     /**
      * 硫化示方书号
      */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.lhNo")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.lhNo", sort = 90)
     @ApiModelProperty(value = "硫化示方书号", name = "lhNo")
     @TableField(value = "LH_NO")
     private String lhNo;
@@ -199,7 +199,7 @@ public class MdmSkuConstructionRef extends BaseEntity {
     /**
      * 硫化示方书类型
      */
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.lhType")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.lhType", sort = 95)
     @ApiModelProperty(value = "硫化示方书类型", name = "lhType")
     @TableField(value = "LH_TYPE")
     private String lhType;
@@ -209,7 +209,7 @@ public class MdmSkuConstructionRef extends BaseEntity {
      */
     @ImportExcelValidated(date = true)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Excel(name = "ui.data.column.mdmSkuConstructionRef.lhReleaseDate", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @Excel(name = "ui.data.column.mdmSkuConstructionRef.lhReleaseDate", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss", sort = 100)
     @ApiModelProperty(value = "硫化示方书发行时间", name = "lhReleaseDate")
     @TableField(value = "LH_RELEASE_DATE")
     private Date lhReleaseDate;
