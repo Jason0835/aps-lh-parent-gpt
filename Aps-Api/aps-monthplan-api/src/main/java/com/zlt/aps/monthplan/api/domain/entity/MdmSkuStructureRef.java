c14c0180a7a78566ef73f1b5b6368aac18a332fe
wengpc <wengpc@zlt.com>
2025-12-30 17:38:08 +0800

    18944 【SKU与结构关系】添加界面和导入界面字段不一致，请核对

--- Aps-Api/aps-monthplan-api/src/main/java/com/zlt/aps/monthplan/api/domain/entity/MdmSkuStructureRef.java
+++ Aps-Api/aps-monthplan-api/src/main/java/com/zlt/aps/monthplan/api/domain/entity/MdmSkuStructureRef.java
@@ -30,13 +30,13 @@ public class MdmSkuStructureRef extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
      /** 工厂编号 */
-//    @Excel(name = "ui.data.column.mdmSkuStructureRef.factoryCode", dictType = "biz_factory_name")
+    @Excel(name = "ui.data.column.mdmSkuStructureRef.factoryCode", dictType = "biz_factory_name", sort = 1)
     @ApiModelProperty(value = "工厂编号", name = "factoryCode")
     @TableField(value = "FACTORY_CODE")
     private String factoryCode;
 
     /** 物料编码 */
-    @Excel(name = "ui.data.column.mdmSkuStructureRef.materialCode", sort = 1)
+    @Excel(name = "ui.data.column.mdmSkuStructureRef.materialCode", sort = 3)
     @ApiModelProperty(value = "物料编码", name = "materialCode")
     @TableField(value = "MATERIAL_CODE")
     private String materialCode;
@@ -48,13 +48,13 @@ public class MdmSkuStructureRef extends BaseEntity {
     private String mesMaterialCode;
 
     /** 结构 */
-    @Excel(name = "ui.data.column.mdmSkuStructureRef.structureName", sort = 4)
+    @Excel(name = "ui.data.column.mdmSkuStructureRef.structureName", sort = 5)
     @ApiModelProperty(value = "结构", name = "structureName")
     @TableField(value = "STRUCTURE_NAME")
     private String structureName;
 
     /** 物料描述 */
-    @Excel(name = "ui.data.column.mdmSkuStructureRef.materialDesc", sort = 3)
+    @Excel(name = "ui.data.column.mdmSkuStructureRef.materialDesc", sort = 4)
     @ApiModelProperty(value = "物料描述", name = "materialDesc")
     @TableField(exist = false)
     private String materialDesc;
