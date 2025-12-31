7c823b6356715a122eb7ccff9d1541da81cdc64f
kf_steve <KF_Steve@zlt.com>
2025-12-31 09:44:49 +0800

    19152 【成型鼓台账】新增 编辑 导入业务主键 （工厂加分类加加名称成型机类型加类型）

--- APS-Modules/aps-maindata/src/main/java/com/zlt/aps/maindata/service/impl/MdmWorkWearInfoServiceImpl.java
+++ APS-Modules/aps-maindata/src/main/java/com/zlt/aps/maindata/service/impl/MdmWorkWearInfoServiceImpl.java
@@ -75,7 +75,7 @@ public class MdmWorkWearInfoServiceImpl extends AbstractDocService<MdmWorkWearIn
     @Override
     protected List<String> getCheckUniqueFields() {
         // 唯一校验字段
-        return new ArrayList<>(Arrays.asList("factoryCode", "workWearName", "specifications", "workWearType"));
+        return new ArrayList<>(Arrays.asList("factoryCode", "workWearName", "cxMachineBrandCode", "workWearType", "cxMachineTypeCode"));
     }
 
     @Override
