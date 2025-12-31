03ca14285f996a3c06184a44f2c24bf6c8ff71dc
sandy <qazwsx@1008>
2025-12-31 10:33:30 +0800

    日产能限制抽象

---
+++ Aps-Api/aps-monthplan-api/src/main/java/com/zlt/aps/monthplan/api/domain/capacity/MpDailyCapacityLimitVo.java
@@ -0,0 +1,44 @@
+package com.zlt.aps.monthplan.api.domain.capacity;
+
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 日产能限制
+ */
+@Data
+public class MpDailyCapacityLimitVo {
+
+    /**
+     * 每日
+     */
+    private Integer dailyDate;
+
+    /**
+     * 最大硫化机台数
+     */
+    private Integer maxLhMachines;
+
+    /**
+     * 最大胎胚种类数
+     */
+    private Integer maxEmbryoTypes;
+
+    /**
+     * 已用硫化机台数
+     */
+    private Integer usedLhMachines;
+
+    /**
+     * 已用胎胚种类数
+     */
+    private Integer usedEmbryoTypes;
+
+    /**
+     *  当日使用的胎胚编码
+     */
+    private Set<String> embryoCodes = new HashSet<>();
+
+}
