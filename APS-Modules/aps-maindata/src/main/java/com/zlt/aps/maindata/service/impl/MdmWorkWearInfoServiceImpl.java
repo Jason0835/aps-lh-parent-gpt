805523a6c121c9de6e259bd8fc72f806389774cd
kf_steve <KF_Steve@zlt.com>
2025-12-31 13:48:01 +0800

    18794 【成型鼓台账】导入和新增必填的校验不一致

--- APS-Modules/aps-maindata/src/main/java/com/zlt/aps/maindata/service/impl/MdmWorkWearInfoServiceImpl.java
+++ APS-Modules/aps-maindata/src/main/java/com/zlt/aps/maindata/service/impl/MdmWorkWearInfoServiceImpl.java
@@ -1,23 +1,16 @@
 package com.zlt.aps.maindata.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.ruoyi.api.gateway.system.domain.ImportErrorLog;
 import com.ruoyi.common.constant.UserConstants;
-import com.ruoyi.common.core.web.domain.BaseEntity;
 import com.ruoyi.common.exception.ServiceException;
 import com.ruoyi.common.i18n.utils.I18nUtil;
-import com.tlt.aps.utils.GenerageMapKeyUtils;
-import com.zlt.aps.common.core.constant.ApsConstant;
 import com.zlt.aps.maindata.mapper.MdmMoldingMachineEntityMapper;
 import com.zlt.aps.maindata.service.IMdmWorkWearInfoService;
-import com.zlt.aps.maindata.utils.ScmListUtils;
-import com.zlt.aps.monthplan.api.domain.entity.MdmMoldingMachine;
 import com.zlt.aps.monthplan.api.domain.entity.MdmWorkWearInfo;
 import com.zlt.bill.common.service.AbstractDocService;
 import com.zlt.common.enums.ImportErrorTypeEnums;
 import com.zlt.sysdef.domain.SysDocType;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,8 +19,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * Copyright (c) 2022, All rights reserved。
@@ -79,27 +70,6 @@ public class MdmWorkWearInfoServiceImpl extends AbstractDocService<MdmWorkWearIn
     }
 
     @Override
-    protected Map<Object, Object> getServiceCheckParams(List<MdmWorkWearInfo> list, List<MdmWorkWearInfo> importList) {
-        Map<Object, Object> serviceCheckParams = super.getServiceCheckParams(list, importList);
-        // 查询成型机列表，如果校验使用机型字段是否为编号
-        List<String> machineCodeList = list.stream().map(MdmWorkWearInfo::getUsedType).collect(Collectors.toList());
-        List<List<String>> splitList = ScmListUtils.getSplitList(machineCodeList, 1000);
-        List<MdmMoldingMachine> moldingMachineList = new ArrayList<>();
-        for (List<String> codeList : splitList) {
-            LambdaQueryWrapper<MdmMoldingMachine> queryWrapper = new LambdaQueryWrapper<>();
-            queryWrapper.eq(BaseEntity::getIsDelete, ApsConstant.DEL_FLAG_NORMAL);
-            queryWrapper.in(MdmMoldingMachine::getMachineCode, codeList);
-            moldingMachineList.addAll(moldingMachineEntityMapper.selectList(queryWrapper));
-        }
-        Map<String, MdmMoldingMachine> moldingMachineMap;
-        if (CollectionUtils.isNotEmpty(moldingMachineList)) {
-            moldingMachineMap = moldingMachineList.stream().collect(Collectors.toMap(item -> GenerageMapKeyUtils.createMapKey(item.getFactoryCode(), item.getMachineCode()), Function.identity(), (v1, v2) -> v1));
-            serviceCheckParams.put("moldingMachineMap", moldingMachineMap);
-        }
-        return serviceCheckParams;
-    }
-
-    @Override
     protected Boolean serviceCheckAndDataHandle(MdmWorkWearInfo importDocEntity, List<ImportErrorLog> importErrorLogs, Long importLogId, int errorRowNum, Map<Object, Object> serviceCheckParams) {
         Integer perimeterMin = importDocEntity.getPerimeterMin();
         Integer perimeterMax = importDocEntity.getPerimeterMax();
@@ -107,15 +77,6 @@ public class MdmWorkWearInfoServiceImpl extends AbstractDocService<MdmWorkWearIn
             String message = I18nUtil.getMessage("ui.data.alert.mdmWorkWearInfo.perimeterCheck");
             com.zlt.common.utils.ImportExcelValidatedUtils.addImportErrorLog(importLogId, ImportErrorTypeEnums.OTHERS.getCode(), errorRowNum, message, importErrorLogs);
             return Boolean.FALSE;
-        }
-        if (serviceCheckParams.containsKey("moldingMachineMap")) {
-            Map<String, MdmMoldingMachine> moldingMachineMap = (Map<String, MdmMoldingMachine>) serviceCheckParams.get("moldingMachineMap");
-            String mapKey = GenerageMapKeyUtils.createMapKey(importDocEntity.getFactoryCode(), importDocEntity.getUsedType());
-            if (!moldingMachineMap.containsKey(mapKey)) {
-                String message = I18nUtil.getMessage("ui.data.alert.mdmWorkWearInfo.useTypeNotExists");
-                com.zlt.common.utils.ImportExcelValidatedUtils.addImportErrorLog(importLogId, ImportErrorTypeEnums.OTHERS.getCode(), errorRowNum, message, importErrorLogs);
-                return Boolean.FALSE;
-            }
         }
         return super.serviceCheckAndDataHandle(importDocEntity, importErrorLogs, importLogId, errorRowNum, serviceCheckParams);
     }
