b79ab9b353723e0f44e70317681fff0b157ec98d
kf_hak <KF_Hak@zlt.com>
2025-12-31 13:58:27 +0800

    增加itf对接系统编码常量

---
+++ APS-Modules/aps-itf/src/main/java/com/zlt/aps/itf/constant/SysCode.java
@@ -0,0 +1,12 @@
+package com.zlt.aps.itf.constant;
+
+/**
+ * 系统编码
+ * @author zlt
+ *
+ */
+public class SysCode {
+	public static final String APS = "APS";
+	public static final String MES = "MES";
+	public static final String SCM = "SCM";
+}
