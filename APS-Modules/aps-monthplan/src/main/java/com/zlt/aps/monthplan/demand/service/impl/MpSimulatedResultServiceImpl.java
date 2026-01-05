270af0724bd02e9bd442459bf65c9d4a0afcb638
yelq <yelinqing.hi@163.com>
2026-01-05 15:31:14 +0800

    19244 【产量预测】需求计划没合并

--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/demand/service/impl/MpSimulatedResultServiceImpl.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/demand/service/impl/MpSimulatedResultServiceImpl.java
@@ -870,7 +870,7 @@ public class MpSimulatedResultServiceImpl extends AbstractDocService<MpSimulated
             return BigDecimal.ZERO.intValue();
         }
         List<MdmProductStock> finishedProductStocks = finishedProductStockMap.get(groupKey);
-        return finishedProductStocks.stream().mapToInt(MdmProductStock::getStockQty).sum();
+        return finishedProductStocks.stream().filter(item -> null != item.getStockQty()).mapToInt(MdmProductStock::getStockQty).sum();
     }
 
     private int calculateRemainingQty(Map<String, List<MdmProductStock>> finishedProductStockMap, String groupKey) {
