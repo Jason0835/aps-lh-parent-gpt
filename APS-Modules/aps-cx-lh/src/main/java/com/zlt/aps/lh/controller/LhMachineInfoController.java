d5ff64009380daa3c775c468f8425df29471ee4b
kf_steve <KF_Steve@zlt.com>
2026-01-07 10:30:35 +0800

    19315 【设备停机计划】选择机台.,10笔/页的数据取值不对

--- APS-Modules/aps-cx-lh/src/main/java/com/zlt/aps/lh/controller/LhMachineInfoController.java
+++ APS-Modules/aps-cx-lh/src/main/java/com/zlt/aps/lh/controller/LhMachineInfoController.java
@@ -21,7 +21,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -178,7 +177,7 @@ public class LhMachineInfoController extends AbstractDocBizController<LhMachineI
 
     @Override
     protected String getOrderBy() {
-        return "create_time desc";
+        return "create_time, id desc";
     }
 
 }
