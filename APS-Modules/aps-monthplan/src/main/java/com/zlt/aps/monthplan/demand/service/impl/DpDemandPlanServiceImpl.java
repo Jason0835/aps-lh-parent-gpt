ff9bb0e558186366c634089a33964eb5ae411e39
kf_nick <kf_nick@qq.com>
2026-01-09 16:12:56 +0800

    19409 净需求计划----->点击生成需求计划需要弹框获取需求计划版本号，然后允许用户修改需求计划版本号

--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/demand/service/impl/DpDemandPlanServiceImpl.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/demand/service/impl/DpDemandPlanServiceImpl.java
@@ -86,7 +86,7 @@ import com.ruoyi.common.exception.ServiceException;
 @Transactional(rollbackFor = Exception.class)
 @RequiredArgsConstructor
 public class DpDemandPlanServiceImpl extends AbstractDocService<DpDemandPlan>  implements IDpDemandPlanService {
-    private static final String PREFIX = "REQ";
+    public static final String PREFIX = "REQ";
     private final static String ZERO_YEAR_WEEK = "0000";
     private final DpDemandPlanEntityMapper demandPlanEntityMapper;
     private final MpFactoryProductionVersionMapper factoryProductionVersionMapper;
@@ -159,6 +159,10 @@ public class DpDemandPlanServiceImpl extends AbstractDocService<DpDemandPlan>  i
 
         // 2. 生成版本号
         String monthPlanVersion = requirementVersionService.generateVersion(PREFIX);
+        if (createCondition.getMonthPlanVersion() != null) {
+            // 19409 净需求计划----->点击生成需求计划需要弹框获取需求计划版本号，然后允许用户修改需求计划版本号
+            monthPlanVersion = createCondition.getMonthPlanVersion();
+        }
         createCondition.setMonthPlanVersion(monthPlanVersion);
 
         // 3. 并行获取数据
