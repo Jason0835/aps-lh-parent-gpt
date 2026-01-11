e825c2565876e4c318e4de709e47eb7b88c76d6a
wengpc <wengpc@zlt.com>
2026-01-11 10:35:10 +0800

    19466 【月度计划排产-算法】修改参数SYS0202002，从S修改成A，现在日硫化产能，还是取标准产能，而不是APS计算的产能（2025年5月）

--- APS-Modules/aps-maindata/src/main/java/com/zlt/aps/maindata/service/impl/MdmSkuLhCapacityServiceImpl.java
+++ APS-Modules/aps-maindata/src/main/java/com/zlt/aps/maindata/service/impl/MdmSkuLhCapacityServiceImpl.java
@@ -1,15 +1,18 @@
 package com.zlt.aps.maindata.service.impl;
 
 import com.ruoyi.common.constant.UserConstants;
+import com.ruoyi.common.core.web.domain.AjaxResult;
 import com.ruoyi.common.i18n.utils.I18nUtil;
+import com.ruoyi.common.text.Convert;
+import com.zlt.aps.common.core.constant.ApsConstant;
 import com.zlt.aps.maindata.service.IMdmSkuLhCapacityService;
 import com.zlt.aps.monthplan.api.domain.entity.MdmSkuLhCapacity;
+import com.zlt.common.utils.PubUtil;
 import com.zlt.sysdef.domain.SysDocType;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -46,6 +49,17 @@ public class MdmSkuLhCapacityServiceImpl extends AbstractDocService<MdmSkuLhCapa
     }
 
     @Override
+    public int save(MdmSkuLhCapacity docEntityVO) {
+        docEntityVO.setBaseVale(null);
+        super.checkUnique(docEntityVO);
+        // 计算APS日硫化量
+        List<MdmSkuLhCapacity> mdmSkuLhCapacityList = new ArrayList<>();
+        mdmSkuLhCapacityList.add(docEntityVO);
+        calculateApsCapacity(mdmSkuLhCapacityList);
+        return baseDao.save(docEntityVO);
+    }
+
+    @Override
     public String checkUnique(MdmSkuLhCapacity docEntityVO) {
         String unique = super.checkUnique(docEntityVO);
         if (UserConstants.NOT_UNIQUE.equals(unique)) {
@@ -59,4 +73,41 @@ public class MdmSkuLhCapacityServiceImpl extends AbstractDocService<MdmSkuLhCapa
         // 唯一校验字段
         return new ArrayList<>(Arrays.asList("factoryCode", "materialCode"));
     }
+
+
+    @Override
+    public AjaxResult importData(List<MdmSkuLhCapacity> list, boolean updateSupport, Long importLogId) {
+        // 计算APS日硫化量
+        calculateApsCapacity(list);
+        return super.importData(list, updateSupport, importLogId);
+    }
+
+    /**
+     * 计算APS日硫化量
+     */
+    private void calculateApsCapacity(List<MdmSkuLhCapacity> sourceList) {
+        if (PubUtil.isEmpty(sourceList)) {
+            return;
+        }
+        // 计算APS日硫化量：APS日硫化量 = 24 * 60 / 硫化总时间(min)
+        sourceList.stream()
+                .filter(skuCapacity -> {
+                    Integer sum = skuCapacity.getSumVulcanization();
+                    return sum != null && sum > 0;
+                })
+                .forEach(skuCapacity -> {
+                    Integer sumVulcanization = skuCapacity.getSumVulcanization();
+                    double divisionResult = (double) ApsConstant.MINUTES_PER_DAY / sumVulcanization;
+                    double ceilResult = Math.ceil(divisionResult);
+                    skuCapacity.setApsCapacity(Convert.toInt(ceilResult));
+                });
+        // 设置默认值
+        sourceList.stream()
+                .filter(skuCapacity -> {
+                    Integer sum = skuCapacity.getSumVulcanization();
+                    return sum == null || sum <= 0;
+                })
+                .forEach(skuCapacity -> skuCapacity.setApsCapacity(0));
+    }
+
 }
