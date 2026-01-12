e8b2e12b91a1665c960d84b8f1f730d1f541cf0f
wengpc <wengpc@zlt.com>
2026-01-12 13:40:44 +0800

    增加获取结构内调整记录版本号接口

--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/adjust/mapper/MpAdjustStructureInEntityMapper.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/adjust/mapper/MpAdjustStructureInEntityMapper.java
@@ -4,6 +4,8 @@ import com.zlt.aps.monthplan.api.domain.entity.MpAdjustStructureIn;
 import com.zlt.core.dao.basemapper.CommBaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * Copyright (c) 2022, All rights reserved。
  * 文件名称：MpAdjustStructureInMapper.java
@@ -19,5 +21,13 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface MpAdjustStructureInEntityMapper extends CommBaseMapper<MpAdjustStructureIn> {
+
+    /**
+     * 查询版本列表
+     * @param queryVO 查询参数
+     * @return 结果
+     */
+    List<MpAdjustStructureIn> getVersionList(MpAdjustStructureIn queryVO);
+
 
 }
