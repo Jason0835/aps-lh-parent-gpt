f52831f723e74302371bdb2330f8d96ba98efa13
wengpc <wengpc@zlt.com>
2026-01-15 13:46:44 +0800

    结构内调整记录表增加字段：是否SKU新增

--- Aps-Api/aps-monthplan-api/src/main/java/com/zlt/aps/monthplan/api/domain/vo/MpAdjustDetailVo.java
+++ Aps-Api/aps-monthplan-api/src/main/java/com/zlt/aps/monthplan/api/domain/vo/MpAdjustDetailVo.java
@@ -25,8 +25,4 @@ public class MpAdjustDetailVo extends MpAdjustStructureIn {
     @TableField(exist = false)
     private String isTrial;
 
-    @ApiModelProperty(value = "是否SKU新增")
-    @TableField(exist = false)
-    private String isSkuAdd;
-
 }
