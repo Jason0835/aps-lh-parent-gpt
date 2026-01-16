bfb5d7b7d4e37abd8b87f7d4d7a6ec8ef8ac83b0
yelq <yelinqing.hi@163.com>
2026-01-16 16:02:58 +0800

    19614 【产量预测】列表只显示最新版本的数据 要查旧版本按条件查询

--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/demand/service/impl/MpProductionPredictionServiceImpl.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/demand/service/impl/MpProductionPredictionServiceImpl.java
@@ -1,7 +1,6 @@
 package com.zlt.aps.monthplan.demand.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -168,15 +167,6 @@ public class MpProductionPredictionServiceImpl extends AbstractDocService<MpProd
         wrapper.eq(MpProductionPrediction::getMonth, queryCondition.getMonth());
         wrapper.eq(MpProductionPrediction::getIsDelete, YesOrNoEnum.NO.getValue());
         wrapper.isNotNull(MpProductionPrediction::getPredictionVersion);
-        wrapper.inSql(MpProductionPrediction::getPredictionVersion,
-            "SELECT MAX(PREDICTION_VERSION) FROM T_MP_PRODUCTION_PREDICTION " +
-                "WHERE (MONTH_PLAN_VERSION, PREDICTION_VERSION) IN (" +
-                "   SELECT MONTH_PLAN_VERSION, MAX(PREDICTION_VERSION) " +
-                "   FROM T_MP_PRODUCTION_PREDICTION " +
-                "   GROUP BY MONTH_PLAN_VERSION" +
-                ") " +
-                "GROUP BY MONTH_PLAN_VERSION, PREDICTION_VERSION"
-        );
         wrapper.orderByDesc(MpProductionPrediction::getPredictionVersion);
         List<MpProductionPrediction> list =  this.mpProductionPredictionEntityMapper.selectList(wrapper);
         if(CollectionUtils.isEmpty(list)){
