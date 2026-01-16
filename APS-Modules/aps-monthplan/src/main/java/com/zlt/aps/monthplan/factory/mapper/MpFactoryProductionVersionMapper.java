d22f0cde1fc2c23358ba3b8a77a3564e9f0efa22
kf_steve <KF_Steve@zlt.com>
2026-01-16 11:11:34 +0800

    月度生产计划版本删除逻辑添加判断，如果只有最后一条版本，不删除版本表数据

--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/factory/mapper/MpFactoryProductionVersionMapper.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/factory/mapper/MpFactoryProductionVersionMapper.java
@@ -52,6 +52,29 @@ public interface MpFactoryProductionVersionMapper extends CommBaseMapper<MpFacto
     int deletedByProductionVersion(FactoryProductionParamVo factoryProductionParam);
 
     /**
+     * 根据分厂、年份、月份。需求版本，排产版本，删除对应的排产版本计划（针对只有最后一个版本的情况，不能直接删除版本表的数据）
+     * t_mp_proc_version
+     * t_mp_proc_month_plan_init
+     * t_mp_proc_no_production_record
+     * t_mp_proc_no_production_plan
+     * t_mp_moulding_day_result
+     * t_mp_moulding_day_result_detail
+     * t_mp_moulding_helper
+     *
+     * @param factoryProductionParam
+     * @return
+     */
+    int deletedLastVersionByProductionVersion(FactoryProductionParamVo factoryProductionParam);
+
+    /**
+     * 根据分厂、年份、月份。需求版本，排产版本，查询对应的排产版本计划数量
+     *
+     * @param factoryProductionParam 参数
+     * @return 结果
+     */
+    int selectCountByProductionVersion(FactoryProductionParamVo factoryProductionParam);
+
+    /**
      * 根据工厂、年份、月份、需求版本删除对应的版本版本数据
      * t_mp_proc_version 版本记录保留一条，清除排产版本信息，其它排产版本记录删除
      * t_mp_proc_month_plan_init 初始化需求版本的所有删除
