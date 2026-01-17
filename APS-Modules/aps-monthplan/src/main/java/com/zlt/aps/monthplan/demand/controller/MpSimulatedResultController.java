7ab02ba5169b29a1e3c02b950f4ba349d46fa4b3
Abdul.Wu <wushuigen312@163.com>
2026-01-17 10:42:42 +0800

    锁还原

--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/demand/controller/MpSimulatedResultController.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/demand/controller/MpSimulatedResultController.java
@@ -2,55 +2,42 @@ package com.zlt.aps.monthplan.demand.controller;
 
 import cn.hutool.core.date.DateUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.ruoyi.api.gateway.system.domain.vo.ImportContext;
+import com.ruoyi.common.core.web.domain.AjaxResult;
+import com.ruoyi.common.core.web.page.TableDataInfo;
+import com.ruoyi.common.log.annotation.Log;
+import com.ruoyi.common.log.enums.BusinessType;
 import com.tlt.aps.redissonLock.annotation.RedissonLockAnno;
 import com.zlt.aps.monthplan.api.domain.entity.MpSimulatedResult;
 import com.zlt.aps.monthplan.demand.mapper.MpSimulatedResultEntityMapper;
 import com.zlt.aps.monthplan.demand.service.IMpSimulatedResultService;
+import com.zlt.bill.common.controller.AbstractDocBizController;
+import com.zlt.bill.common.service.IDocService;
 import com.zlt.common.utils.PubUtil;
-
-import com.ruoyi.api.gateway.system.domain.vo.ImportContext;
-import lombok.extern.slf4j.Slf4j;
-import com.ruoyi.common.core.web.domain.AjaxResult;
-
-import com.ruoyi.common.log.annotation.Log;
-import com.ruoyi.common.log.enums.BusinessType;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-
-import com.ruoyi.common.core.web.page.TableDataInfo;
-
-import com.zlt.bill.common.controller.AbstractDocBizController;
-import com.zlt.bill.common.service.IDocService ;
-
 /**
-* Copyright (c) 2022, All rights reserved。
-* 文件名称：MpSimulatedResultController.java
-* 描    述：S2-1004.实单模拟排产 控制层类：....
-*@author yelq
-*@date 2025-12-31
-*@version 1.0
-*
- *  修改记录：
-*     修改时间：...
-*     修 改 人：yelq
-*     修改内容：...
-*/
+ * Copyright (c) 2022, All rights reserved。
+ * 文件名称：MpSimulatedResultController.java
+ * 描    述：S2-1004.实单模拟排产 控制层类：....
+ *
+ * @author yelq
+ * @version 1.0
+ * <p>
+ * 修改记录：
+ * 修改时间：...
+ * 修 改 人：yelq
+ * 修改内容：...
+ * @date 2025-12-31
+ */
 @Slf4j
 @Api(tags = "S2-1004.实单模拟排产")
 @RestController
@@ -85,7 +72,7 @@ public class MpSimulatedResultController extends AbstractDocBizController<MpSimu
     @ApiOperation("保存")
     @PostMapping("/save")
     @Override
-    public AjaxResult save(@RequestBody MpSimulatedResult billVO){
+    public AjaxResult save(@RequestBody MpSimulatedResult billVO) {
         return super.save(billVO);
     }
 
@@ -96,7 +83,7 @@ public class MpSimulatedResultController extends AbstractDocBizController<MpSimu
     @ApiOperation("删除")
     @DeleteMapping("/remove")
     @Override
-    public AjaxResult removeByIds(@RequestBody List<Long> ids){
+    public AjaxResult removeByIds(@RequestBody List<Long> ids) {
         return super.removeByIds(ids);
     }
 
@@ -114,6 +101,7 @@ public class MpSimulatedResultController extends AbstractDocBizController<MpSimu
 
     /**
      * 根据集合导入S2-1004.实单模拟排产数据
+     *
      * @param importContext 导入上下文
      * @param updateSupport 已存在记录是否更新
      * @return 结果
@@ -123,7 +111,7 @@ public class MpSimulatedResultController extends AbstractDocBizController<MpSimu
     @PostMapping("/importData")
     @Override
     public AjaxResult importData(@RequestBody ImportContext importContext, @RequestParam("updateSupport") boolean updateSupport) throws Exception {
-        return super.importData(importContext,updateSupport);
+        return super.importData(importContext, updateSupport);
     }
 
     /**
@@ -142,12 +130,12 @@ public class MpSimulatedResultController extends AbstractDocBizController<MpSimu
      * 实单模拟排产
      */
     @ApiOperation("实单模拟排产")
-//    @RedissonLockAnno(uniqueMark = "redissonLock:simulatedResult:createVmMonthPrediction:",
-//        expressions = {"#createCondition.factoryCode", "#createCondition.year", "#createCondition.month"},
-//        msgKey = "ui.data.alert.createVmMonthPrediction.run",
-//        waitTime = 5,
-//        leaseTime = 300
-//    )
+    @RedissonLockAnno(uniqueMark = "redissonLock:simulatedResult:createVmMonthPrediction:",
+            expressions = {"#createCondition.factoryCode", "#createCondition.year", "#createCondition.month"},
+            msgKey = "ui.data.alert.createVmMonthPrediction.run",
+            waitTime = 5,
+            leaseTime = 300
+    )
     @PostMapping("/createVmMonthPrediction")
     public AjaxResult createVmMonthPrediction(@RequestBody MpSimulatedResult createCondition) throws InterruptedException {
         return this.mpSimulatedResultService.createVmMonthPrediction(createCondition);
@@ -162,17 +150,18 @@ public class MpSimulatedResultController extends AbstractDocBizController<MpSimu
 
     /**
      * 翻译列表
+     *
      * @param resultList
      */
     private List<MpSimulatedResult> translationList(List<MpSimulatedResult> resultList) {
-        for (MpSimulatedResult item: resultList) {
+        for (MpSimulatedResult item : resultList) {
             item.setUpdateDate(DateUtil.formatDateTime(item.getUpdateTime()));
         }
         return resultList;
     }
 
     @Override
-    protected IDocService getDocService(){
+    protected IDocService getDocService() {
         return mpSimulatedResultService;
     }
 
@@ -233,7 +222,7 @@ public class MpSimulatedResultController extends AbstractDocBizController<MpSimu
 
 
     @Override
-    protected String getTypeCode(){
+    protected String getTypeCode() {
         return "2025123114";
     }
 
