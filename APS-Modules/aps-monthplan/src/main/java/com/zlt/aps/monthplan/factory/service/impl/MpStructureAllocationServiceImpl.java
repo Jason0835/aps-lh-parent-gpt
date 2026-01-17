48570c1b0dea6e9bddec81f37f083143001a5275
wengpc <wengpc@zlt.com>
2026-01-17 21:44:03 +0800

    【周程滚动调整】结构外调整增加获取下一个结构功能调整

--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/factory/service/impl/MpStructureAllocationServiceImpl.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/factory/service/impl/MpStructureAllocationServiceImpl.java
@@ -126,7 +126,9 @@ public class MpStructureAllocationServiceImpl extends AbstractDocService<MpStruc
                         Comparator.nullsLast(Integer::compareTo)));
         // 从集合中找出日期最接近目标开始日期和结束日期的数据
         MpStructureAllocation structureAllocation = getClosestStructureAllocation(structureAllocationList, param.getId(), beginDay, endDay);
-        structureAllocation.setBeginDay(adjustEndDay);
+        if (structureAllocation != null) {
+            structureAllocation.setBeginDay(adjustEndDay);
+        }
         return structureAllocation;
     }
 
