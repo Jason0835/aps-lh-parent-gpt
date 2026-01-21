4be983860fde569f3a9c127242badc5b626a987f
yelq <yelinqing.hi@163.com>
2026-01-21 16:38:07 +0800

    添加实单模拟异步进程执行，修改超时时长为24分钟

--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/demand/service/impl/MpSimulatedResultServiceImpl.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/demand/service/impl/MpSimulatedResultServiceImpl.java
@@ -114,7 +114,7 @@ public class MpSimulatedResultServiceImpl extends AbstractDocService<MpSimulated
 
     @Override
     public DeferredResult<Object> createVmMonthPrediction(MpSimulatedResult createCondition) {
-        DeferredResult<Object> result = new DeferredResult<>(480000L);
+        DeferredResult<Object> result = new DeferredResult<>(1440000L);
         try {
             AsyncVo<String, Object> request = new AsyncVo<>();
             request.setResult(result);
