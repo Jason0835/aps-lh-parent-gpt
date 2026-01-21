ebd202c1d305a1ed8519f711b0c8346f54c1133a
yelq <yelinqing.hi@163.com>
2026-01-21 11:26:01 +0800

    19330 【模具排产-算法】没有生成未排产结果表

--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/factory/service/IMonthPlanNoProductionPlanService.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/factory/service/IMonthPlanNoProductionPlanService.java
@@ -2,32 +2,20 @@ package com.zlt.aps.monthplan.factory.service;
 
 
 import com.zlt.aps.monthplan.api.domain.entity.MonthPlanNoProductionPlan;
-import com.zlt.aps.monthplan.api.domain.vo.MonthPlanStatisticsVo;
-
-import java.util.List;
-
+import com.zlt.bill.common.service.IDocService;
 /**
  * Copyright (c) 2022, All rights reserved。
  * 文件名称：IMonthPlanNoProductionPlanService.java
- * 描    述：IMonthPlanNoProductionPlanService分厂月生产计划排产过程-未排产计划后端接口
- *@author zlt
- *@date 2025-03-21
+ * 描    述：IMonthPlanNoProductionPlanServiceS2-0606.排产结果-未排产计划后端接口
+ *@author yelq
+ *@date 2026-01-21
  *@version 1.0
  *
  *  修改记录：
  *     修改时间：...
- *     修 改 人：zlt
+ *     修 改 人：yelq
  *     修改内容：...
  */
-public interface IMonthPlanNoProductionPlanService{
+public interface IMonthPlanNoProductionPlanService  extends IDocService<MonthPlanNoProductionPlan>{
 
-    /**
-     * 列表查询
-     */
-    List<MonthPlanNoProductionPlan> selectList(MonthPlanNoProductionPlan query);
-
-    /**
-     * 统计未排SAP总量
-     */
-    void statistics(MonthPlanStatisticsVo statisticsVo, MonthPlanNoProductionPlan noProductionPlan);
 }
