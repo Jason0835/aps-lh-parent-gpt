3465a1e820cf27a49568367670f5b6a6636bbe6a
wengpc <wengpc@zlt.com>
2026-01-21 12:36:24 +0800

    19733 【周程滚动调整】结构内-获取调整订单 （月度计划有，订单无的 未生成调整订单）

--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/adjust/service/impl/MpAdjustStructureInStrategy.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/adjust/service/impl/MpAdjustStructureInStrategy.java
@@ -180,54 +180,6 @@ public class MpAdjustStructureInStrategy extends AbstractBaseWeekAdjustService {
 
     }
 
-    /**
-     * 构建结构内调整明细（月度计划有，无订单）
-     * @param contextDTO
-     * @return
-     */
-    private List<MpAdjustDetailVo> buildAdjustDetailByMonthPlanList(MpRollAdjustContextDTO contextDTO) {
-        // 销售订单池列表
-        List<SalesOrderPool> salesOrderPoolList = contextDTO.getSalesOrderPoolList();
-        // 试制量试计划列表
-        List<MpTrialPlan> trialPlanList = contextDTO.getMpTrialPlanList();
-        // 月度生产计划列表
-        List<FactoryMonthPlanFinalAdjustVo> monthPlanProdList = contextDTO.getFactoryMonthPlanProdFinalList();
-        // 结果集初始化
-        List<MpAdjustDetailVo> resultList = new ArrayList<>();
-        // 列表为空则直接返回空结果
-        if (PubUtil.isEmpty(monthPlanProdList)) {
-            return resultList;
-        }
-        // 销售订单池列表
-        Set<String> salesOrderSet = salesOrderPoolList.stream()
-                .map(SalesOrderPool::getOriMaterialCode)
-                .collect(Collectors.toSet());
-        // 试制量试计划列表
-        Set<String> trialPlanSet = trialPlanList.stream()
-                .map(MpTrialPlan::getMaterialCode)
-                .collect(Collectors.toSet());
-        // 遍历月度生产计划
-        for (FactoryMonthPlanFinalAdjustVo monthPlan : monthPlanProdList) {
-            String materialCode = monthPlan.getMaterialCode();
-            // 物料编码为空则跳过
-            if (StringUtils.isEmpty(materialCode)) {
-                continue;
-            }
-            // 存在订单或者试制量试跳过
-            if (salesOrderSet.contains(materialCode) || trialPlanSet.contains(materialCode)) {
-                continue;
-            }
-            // 创建基础通用字段
-            MpAdjustDetailVo adjustDetailVo = createBaseMpAdjustDetailVo(contextDTO, materialCode, 0, ApsConstant.FALSE);
-            // 设置月度生产计划关联的字段
-            setPlanRelatedFields(contextDTO, adjustDetailVo, monthPlan);
-            // 调整前净需求量
-            setPreviousNetQty(adjustDetailVo, monthPlan);
-            // 添加到结果集
-            resultList.add(adjustDetailVo);
-        }
-        return resultList;
-    }
 
     /**
      * 构建结构内调整明细（试制量试计划）
