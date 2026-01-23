f66fdb6d969b974d193873c171f917e78737efd3
kf_nick <kf_nick@qq.com>
2026-01-23 16:47:44 +0800

    原材料需求调整NICK +

--- APS-Modules/aps-maindata/src/main/java/com/zlt/aps/maindata/service/impl/RawSpecialMaterialRatioServiceImpl.java
+++ APS-Modules/aps-maindata/src/main/java/com/zlt/aps/maindata/service/impl/RawSpecialMaterialRatioServiceImpl.java
@@ -112,6 +112,6 @@ public class RawSpecialMaterialRatioServiceImpl extends AbstractDocService<RawSp
     @Override
     protected List<String> getCheckUniqueFields() {
         // 唯一校验字段
-        return new ArrayList<>(Arrays.asList("factoryCode", "materialCode"));
+        return new ArrayList<>(Arrays.asList("factoryCode", "materialCode", "standardLength"));
     }
 }
