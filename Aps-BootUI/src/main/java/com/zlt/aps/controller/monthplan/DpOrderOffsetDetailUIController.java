2efd1b6eebdc5877a0d1f639141b42ce872a36a1
kf_nick <kf_nick@qq.com>
2026-01-23 18:00:25 +0800

    订单冲减

---
+++ Aps-BootUI/src/main/java/com/zlt/aps/controller/monthplan/DpOrderOffsetDetailUIController.java
@@ -0,0 +1,129 @@
+package com.zlt.aps.controller.monthplan;
+
+import com.ruoyi.common.core.utils.poi.ExcelUtil;
+import com.ruoyi.common.core.web.domain.AjaxResult;
+import com.ruoyi.common.core.web.page.TableDataInfo;
+import com.ruoyi.common.i18n.utils.I18nUtil;
+import com.ruoyi.common.text.Convert;
+import com.ruoyi.common4ui.constant.UserConstants;
+import com.ruoyi.common.exception.ServiceException;
+import com.ruoyi.common4ui.core.controller.BaseUIController;
+import com.zlt.aps.monthplan.api.domain.entity.DpOrderOffsetDetail;
+import com.zlt.aps.monthplan.api.service.IDpOrderOffsetDetailRemoteService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+
+import lombok.extern.slf4j.Slf4j;
+import com.ruoyi.api.gateway.system.domain.vo.ImportContext;
+import com.zlt.file.encryptbyll.FileEncryptUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Copyright (c) 2022, All rights reserved。
+ * 文件名称：DpOrderOffsetDetailUIController.java
+ * 描    述：S1-0604订单冲减分配 UI控制层类：....
+ *@author zlt
+ *@date 2026-01-23
+ *@version 1.0
+ *
+ *  修改记录：
+ *     修改时间：...
+ *     修 改 人：zlt
+ *     修改内容：...
+ */
+@Slf4j
+@Api(tags = "S1-0604订单冲减分配")
+@Controller
+@RequestMapping("/maindata/dpOrderOffsetDetail")
+public class DpOrderOffsetDetailUIController extends BaseUIController<DpOrderOffsetDetail> {
+
+    @Autowired
+    private IDpOrderOffsetDetailRemoteService iDpOrderOffsetDetailService;
+
+    /**
+     * 根据条件查询主表数据
+     */
+    @ApiOperation("根据条件查询主表数据")
+    @RequiresPermissions("maindata:dpOrderOffsetDetail:list")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(DpOrderOffsetDetail dpOrderOffsetDetail) {
+        return iDpOrderOffsetDetailService.list(dpOrderOffsetDetail);
+    }
+
+
+
+    /**
+     * 导出模板文件的文件名，派生类重写名称。
+     * 示例：支持多语言写法： String fileName = I18nUtil.getMessage("ui.cd90.machine.export.fileName");
+     * @return
+     */
+    @Override
+    public String getExportTemplateFileName(){
+        return this.getFunctionName();
+    }
+
+
+    /**
+     * 继承时重写方法。
+     *
+     * @return
+     */
+    @Override
+    public String getProcedureCode() {
+        return "0";
+    }
+
+    /**
+     * 继承时重写方法。
+     *
+     * @return
+     */
+    @Override
+    public String getFunctionName() {
+        return I18nUtil.getMessage("ui.data.column.dpOrderOffsetDetail.modelName");
+    }
+
+    /**
+     * 重写导入模板的生成逻辑
+     */
+    @ApiOperation("下载导入模板")
+    @Override
+    public AjaxResult importTemplate(HttpServletResponse response) throws IOException {
+        String fileName = this.getExportTemplateFileName();
+        ExcelUtil<DpOrderOffsetDetail> util = new ExcelUtil<>(DpOrderOffsetDetail.class);
+        util.exportExcel(response, null, fileName, fileName);
+        return AjaxResult.success();
+    }
+
+    @ApiOperation("数据导出")
+    @GetMapping({"/export"})
+    @ResponseBody
+    @Override
+    public void export(HttpServletResponse response, DpOrderOffsetDetail entity) throws IOException {
+        String fileName = this.getExportTemplateFileName();
+        byte[] excelBytes = iDpOrderOffsetDetailService.exportData(entity,fileName);
+        ByteArrayInputStream in = new ByteArrayInputStream(excelBytes);
+        ExcelUtil.setResponseHeader(response, fileName, ".xlsx");
+        IOUtils.copy(in, response.getOutputStream());
+        response.flushBuffer();
+    }
+}
