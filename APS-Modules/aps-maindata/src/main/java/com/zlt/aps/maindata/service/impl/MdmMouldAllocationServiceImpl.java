40145879d3ad194ae058652790bc9f082deb0fcb
kf_steve <KF_Steve@zlt.com>
2026-01-28 14:42:10 +0800

    20049 【模具分配比例】提示语，不对：116工厂，应该是越南工厂

--- APS-Modules/aps-maindata/src/main/java/com/zlt/aps/maindata/service/impl/MdmMouldAllocationServiceImpl.java
+++ APS-Modules/aps-maindata/src/main/java/com/zlt/aps/maindata/service/impl/MdmMouldAllocationServiceImpl.java
@@ -1,6 +1,8 @@
 package com.zlt.aps.maindata.service.impl;
 
+import com.ruoyi.api.gateway.system.service.ISysDictDataCacheService;
 import com.ruoyi.common.constant.UserConstants;
+import com.ruoyi.common.core.domain.SysDictData;
 import com.ruoyi.common.exception.ServiceException;
 import com.ruoyi.common.i18n.utils.I18nUtil;
 import com.ruoyi.common.utils.StringUtils;
@@ -9,12 +11,15 @@ import com.zlt.aps.monthplan.api.domain.entity.MdmMouldAllocation;
 import com.zlt.bill.common.service.AbstractDocService;
 import com.zlt.sysdef.domain.SysDocType;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Copyright (c) 2022, All rights reserved。
@@ -33,6 +38,10 @@ import java.util.List;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class MdmMouldAllocationServiceImpl extends AbstractDocService<MdmMouldAllocation>  implements IMdmMouldAllocationService {
+
+    @Autowired
+    private ISysDictDataCacheService sysDictDataCacheService;
+
     @Override
     protected String getDocTypeCode() {
         return "MDM0118";
@@ -49,9 +58,15 @@ public class MdmMouldAllocationServiceImpl extends AbstractDocService<MdmMouldAl
     public String checkUnique(MdmMouldAllocation docEntityVO) {
         String unique = super.checkUnique(docEntityVO);
         if (UserConstants.NOT_UNIQUE.equals(unique)) {
+            List<SysDictData> dictDataList = sysDictDataCacheService.getType("biz_factory_name");
+            List<SysDictData> dictData = dictDataList.stream().filter(item -> item.getDictValue().equals(docEntityVO.getFactoryCode())).collect(Collectors.toList());
+            String dictLabel = docEntityVO.getFactoryCode();
+            if (CollectionUtils.isNotEmpty(dictData)) {
+                dictLabel = dictData.get(0).getDictLabel();
+            }
             String message = StringUtils.format(I18nUtil.getMessage("ui.data.alert.mdmMouldAllocation.notUnique"),
                     docEntityVO.getSpecifications(), docEntityVO.getMainPattern(),
-                    docEntityVO.getStructureName(), docEntityVO.getFactoryCode(), docEntityVO.getYear(), docEntityVO.getMonth());
+                    docEntityVO.getStructureName(), dictLabel, docEntityVO.getYear(), docEntityVO.getMonth());
             throw new ServiceException(message);
         }
         return unique;
