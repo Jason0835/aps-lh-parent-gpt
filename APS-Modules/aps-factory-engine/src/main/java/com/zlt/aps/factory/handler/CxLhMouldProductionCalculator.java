4780f0543b21a5bd55d8f474bb1495b6fe3c7df3
Abdul.Wu <wushuigen312@163.com>
2026-01-29 16:18:50 +0800

    换活字块当天换条件修改为>=

--- APS-Modules/aps-factory-engine/src/main/java/com/zlt/aps/factory/handler/CxLhMouldProductionCalculator.java
+++ APS-Modules/aps-factory-engine/src/main/java/com/zlt/aps/factory/handler/CxLhMouldProductionCalculator.java
@@ -320,7 +320,7 @@ public class CxLhMouldProductionCalculator {
         //前Sku排产量与前Sku日硫化量的差值
         Integer beforeSkuDiffValue = Math.abs(beforeSkuDayMaxQty - beforeSkuProductionQty);
         //差值 > 参数值，表示可以当天换活字块，排量 = changeTypeBlockMaxQty
-        if (beforeSkuDiffValue > changeTypeBlockQtyDiff) {
+        if (beforeSkuDiffValue >= changeTypeBlockQtyDiff) {
             Integer afterSkuProductionQty = paramConfiguration.getChangeTypeBlockMaxQty();
             //损耗量 = 日硫化量 - 前Sku排产量 - 自己排产量
             lossQty = lossQty - afterSkuProductionQty;
