6797f1378730cef7f1df5c2e352bd57c3188600c
kf_nick <kf_nick@qq.com>
2026-02-02 18:09:16 +0800

    型腔活块的SQL修改，整合试制

--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/raw/service/impl/RawWarningServiceImpl.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/raw/service/impl/RawWarningServiceImpl.java
@@ -87,7 +87,8 @@ public class RawWarningServiceImpl extends ServiceImpl<RawWarningRecordEntityMap
                         I18nUtil.getMessage("raw.warning.no.week.usage.data"),
                         factoryCode, year, week
                 );
-                return -1;
+                log.info(message);
+                return 0;
             }
 
             // 2. 批量获取用量偏差预警配置
