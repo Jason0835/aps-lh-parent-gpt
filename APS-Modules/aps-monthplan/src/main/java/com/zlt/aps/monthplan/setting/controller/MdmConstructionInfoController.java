8d74b05a2ee1c77708b955ab19288884f4f91421
wengpc <wengpc@zlt.com>
2026-02-04 14:53:59 +0800

    20180 【UAT环境】投产胎胚施工，花纹搜索无效

--- APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/setting/controller/MdmConstructionInfoController.java
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/setting/controller/MdmConstructionInfoController.java
@@ -236,6 +236,7 @@ public class MdmConstructionInfoController extends AbstractDocBizController<MdmC
         queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("hydraulicPressureCuringTime")), "HYDRAULIC_PRESSURE_CURING_TIME", queryVO.getFieldValueByFieldName("hydraulicPressureCuringTime"));
         queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("moldCavity")), "MOLD_CAVITY", queryVO.getFieldValueByFieldName("moldCavity"));
         queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("productionStage")), "PRODUCTION_STAGE", queryVO.getFieldValueByFieldName("productionStage"));
+        queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("pattern")), "PATTERN", queryVO.getFieldValueByFieldName("pattern"));
     }
 
 
