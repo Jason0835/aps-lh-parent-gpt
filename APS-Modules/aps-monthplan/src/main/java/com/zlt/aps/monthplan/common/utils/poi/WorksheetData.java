67f8e00827194914d92284ef998ac3ab6a288d76
yelq <yelinqing.hi@163.com>
2026-02-06 16:46:27 +0800

    19849 实单模拟排产----->导出excel需要展示每个月的排产结果表，如下截图的excel

---
+++ APS-Modules/aps-monthplan/src/main/java/com/zlt/aps/monthplan/common/utils/poi/WorksheetData.java
@@ -0,0 +1,19 @@
+package com.zlt.aps.monthplan.common.utils.poi;
+
+import com.zlt.aps.monthplan.api.domain.entity.FactoryMonthPlanMouldDayResult;
+import com.zlt.aps.monthplan.api.domain.entity.MpSimulatedResult;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 多sheet数据
+ * @author Yelq
+ */
+@Data
+public class WorksheetData {
+  private String sheetName;
+  private List<MpSimulatedResult> simulatedResults;
+  private List<FactoryMonthPlanMouldDayResult> mouldDayResults;
+}
+
