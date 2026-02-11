a447bb62ff51e6823f39f0f6a66d977860d4f510
yelq <yelinqing.hi@163.com>
2026-02-11 10:43:48 +0800

    实单模拟储备订单必须结合实单才生成净需求

--- APS-Modules/aps-maindata/src/main/java/com/zlt/aps/maindata/enums/MonthPlanEnums.java
+++ APS-Modules/aps-maindata/src/main/java/com/zlt/aps/maindata/enums/MonthPlanEnums.java
@@ -204,9 +204,9 @@ public enum MonthPlanEnums {
      */
     NET_REQUIREMENT_DAY_CAPACITY("SYS0208001", "净需求计划日产能，计算区域总产能 = 净需求计划日产能 * 当月天数，作用是重新调整每个区域产能"),
     /**
-     *  SYS0206006 EUDR开始的年周号
+     *  SYS0202006 EUDR开始的年周号
      */
-    EUDR_REQUIRE("SYS0206006","EUDR开始的年周号");
+    EUDR_REQUIRE("SYS0202006","EUDR开始的年周号");
 
 
 
