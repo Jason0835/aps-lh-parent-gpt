101ce525383f537c7bea13f6fea821609b979ca3
Abdul.Wu <wushuigen312@163.com>
2026-02-11 20:31:05 +0800

    特殊原材料库存不足，则标记本次轮询跳过，并记录特殊原材料限制。防止死循环

--- APS-Modules/aps-factory-engine/src/main/java/com/zlt/aps/factory/daylimit/MouldProductionLimitTypeEnum.java
+++ APS-Modules/aps-factory-engine/src/main/java/com/zlt/aps/factory/daylimit/MouldProductionLimitTypeEnum.java
@@ -19,59 +19,63 @@ public enum MouldProductionLimitTypeEnum {
     /**
      * 00 无限制
      */
-    NO_LIMIT("00", "无限制",""),
+    NO_LIMIT("00", "无限制", ""),
     /**
      * 01 胎胚种类数限制
      */
-    EMBRYO_CODE_COUNT_LIMIT("01", "胎胚种类数限制","alg.data.mouldProduction.embryoCodeCountLimit"),
+    EMBRYO_CODE_COUNT_LIMIT("01", "胎胚种类数限制", "alg.data.mouldProduction.embryoCodeCountLimit"),
     /**
      * 02 模具排产日限制
      */
-    MOULD_LOCAL_LIMIT("02", "模具排产日限制","alg.data.mouldProduction.mouldLocalLimit"),
+    MOULD_LOCAL_LIMIT("02", "模具排产日限制", "alg.data.mouldProduction.mouldLocalLimit"),
     /**
      * 03 模壳总数限制
      */
-    MOULD_SHELL_LIMIT("03", "模壳总数限制","alg.data.mouldProduction.mouldShellLimit"),
+    MOULD_SHELL_LIMIT("03", "模壳总数限制", "alg.data.mouldProduction.mouldShellLimit"),
     /**
      * 04 模具分配比例限制
      */
-    MOULD_ALLOCATION_LIMIT("04", "模具分配比例限制","alg.data.mouldProduction.mouldAllocationLimit"),
+    MOULD_ALLOCATION_LIMIT("04", "模具分配比例限制", "alg.data.mouldProduction.mouldAllocationLimit"),
     /**
      * 05 胶囊卡盘总数限制
      */
-    CAPSULE_CHUCK_LIMIT("05", "胶囊卡盘总数限制","alg.data.mouldProduction.capsuleChuckLimit"),
+    CAPSULE_CHUCK_LIMIT("05", "胶囊卡盘总数限制", "alg.data.mouldProduction.capsuleChuckLimit"),
     /**
      * 06 胎胚种类数、模具排产日双重限制
      */
-    EMBRYO_COUNT_AND_MOULD_LOCAL_LIMIT("06", "胎胚种类数、模具排产日双重限制","alg.data.mouldProduction.embryoCountAndMouldLocalLimit"),
+    EMBRYO_COUNT_AND_MOULD_LOCAL_LIMIT("06", "胎胚种类数、模具排产日双重限制", "alg.data.mouldProduction.embryoCountAndMouldLocalLimit"),
     /**
      * 07 胎胚种类数、模具排产日、模壳总数多重限制
      */
-    MOULD_SHELL_DOUBLE_LIMIT("07", "胎胚种类数、模具排产日、模壳总数多重限制","alg.data.mouldProduction.mouldShellDoubleLimit"),
+    MOULD_SHELL_DOUBLE_LIMIT("07", "胎胚种类数、模具排产日、模壳总数多重限制", "alg.data.mouldProduction.mouldShellDoubleLimit"),
     /**
      * 08 胎胚种类数、模具排产日、模壳总数、模具分配比例多重限制
      */
-    MOULD_ALLOCATION_DOUBLE_LIMIT("08", "胎胚种类数、模具排产日、模壳总数、模具分配比例多重限制","alg.data.mouldProduction.mouldAllocationDoubleLimit"),
+    MOULD_ALLOCATION_DOUBLE_LIMIT("08", "胎胚种类数、模具排产日、模壳总数、模具分配比例多重限制", "alg.data.mouldProduction.mouldAllocationDoubleLimit"),
     /**
      * 09 胎胚种类数、模具排产日、模壳总数、模具分配比例、胶囊卡盘总数多重限制
      */
-    CAPSULE_CHUCK_DOUBLE_LIMIT("09", "胎胚种类数、模具排产日、模壳总数、模具分配比例、胶囊卡盘总数多重限制","alg.data.mouldProduction.capsuleChuckDoubleLimit"),
+    CAPSULE_CHUCK_DOUBLE_LIMIT("09", "胎胚种类数、模具排产日、模壳总数、模具分配比例、胶囊卡盘总数多重限制", "alg.data.mouldProduction.capsuleChuckDoubleLimit"),
     /**
      * 10 换模次数限制
      */
-    CHANGE_MOULD_LIMIT("09", "换模次数限制","alg.data.mouldProduction.changeMouldLimit"),
+    CHANGE_MOULD_LIMIT("09", "换模次数限制", "alg.data.mouldProduction.changeMouldLimit"),
     /**
      * 11 日产能上限限制
      */
-    DAY_CAPACITY_LIMIT("11", "日产能上限限制","alg.data.mouldProduction.dayCapacityLimit"),
+    DAY_CAPACITY_LIMIT("11", "日产能上限限制", "alg.data.mouldProduction.dayCapacityLimit"),
     /**
      * 12 没有模具限制
      */
-    FIND_MOULD_LIMIT("12", "没有模具限制","alg.data.mouldProduction.enableMouldLimit"),
+    FIND_MOULD_LIMIT("12", "没有模具限制", "alg.data.mouldProduction.enableMouldLimit"),
     /**
      * 16 胎胚种类数、模具排产日、模壳总数、模具分配比例、胶囊卡盘总数、日产能上限多重限制
      */
-    DAY_CAPACITY_DOUBLE_LIMIT("16", "胎胚种类数、模具排产日、模壳总数、模具分配比例、胶囊卡盘总数、日产能上限多重限制","alg.data.mouldProduction.dayCapacityDoubleLimit");
+    DAY_CAPACITY_DOUBLE_LIMIT("16", "胎胚种类数、模具排产日、模壳总数、模具分配比例、胶囊卡盘总数、日产能上限多重限制", "alg.data.mouldProduction.dayCapacityDoubleLimit"),
+    /**
+     * 17 特殊原材料库存限制
+     */
+    SPECIAL_MATERIAL_STOCK_LIMIT("17", "特殊原材料库存限制", "alg.data.mouldProduction.specialMaterialStockLimit");
     /**
      * 限制类型
      */
