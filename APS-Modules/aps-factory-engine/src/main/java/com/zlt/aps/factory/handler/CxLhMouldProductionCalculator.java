17a3b3ed1b595f64a00acbd87620463f2cb73cc1
Abdul.Wu <wushuigen312@163.com>
2026-02-11 14:45:05 +0800

    去除无效引用

--- APS-Modules/aps-factory-engine/src/main/java/com/zlt/aps/factory/handler/CxLhMouldProductionCalculator.java
+++ APS-Modules/aps-factory-engine/src/main/java/com/zlt/aps/factory/handler/CxLhMouldProductionCalculator.java
@@ -1,6 +1,5 @@
 package com.zlt.aps.factory.handler;
 
-import com.tlt.aps.constant.StringConstant;
 import com.tlt.aps.enums.YesOrNoEnum;
 import com.zlt.aps.factory.check.SkuSecondChecker;
 import com.zlt.aps.factory.constant.ProductionConstant;
