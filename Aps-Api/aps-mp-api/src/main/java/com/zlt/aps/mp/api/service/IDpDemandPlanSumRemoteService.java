9b087b4b8849c8a832a2657201f4deded33971cd
Abdul.Wu <wushuigen312@163.com>
2026-03-02 18:10:00 +0800

    净需求计划增加统计接口

--- Aps-Api/aps-mp-api/src/main/java/com/zlt/aps/mp/api/service/IDpDemandPlanSumRemoteService.java
+++ Aps-Api/aps-mp-api/src/main/java/com/zlt/aps/mp/api/service/IDpDemandPlanSumRemoteService.java
@@ -4,25 +4,28 @@ import com.ruoyi.common.constant.ServiceNameConstants;
 import com.ruoyi.common.core.web.domain.AjaxResult;
 import com.ruoyi.common.core.web.page.TableDataInfo;
 import com.zlt.aps.mp.api.domain.entity.DpDemandPlanSum;
-import org.springframework.cloud.openfeign.FeignClient;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 /**
  * Copyright (c) 2022, All rights reserved。
  * 文件名称：IDpDemandPlanSumRemoteService.java
  * 描    述：IDpDemandPlanSumRemoteService需求计划汇总前端接口
- *@author yelq
- *@date 2026-01-22
- *@version 1.0
  *
- *  修改记录：
- *     修改时间：...
- *     修 改 人：yelq
- *     修改内容：...
+ * @author yelq
+ * @version 1.0
+ * <p>
+ * 修改记录：
+ * 修改时间：...
+ * 修 改 人：yelq
+ * 修改内容：...
+ * @date 2026-01-22
  */
-@FeignClient(contextId = "IDpDemandPlanSumRemoteService", value =ServiceNameConstants.GATEWAY_SERVICE,path = "${api.path.monthplan:/monthplan}")
+@FeignClient(contextId = "IDpDemandPlanSumRemoteService", value = ServiceNameConstants.GATEWAY_SERVICE, path = "${api.path.monthplan:/monthplan}")
 public interface IDpDemandPlanSumRemoteService {
 
     /**
@@ -31,18 +34,31 @@ public interface IDpDemandPlanSumRemoteService {
     @ApiOperation("查询列表")
     @PostMapping("/demandPlanSum/list")
     TableDataInfo list(@RequestBody DpDemandPlanSum QueryVO);
+
+    /**
+     * 查询统计数据
+     *
+     * @param queryCondition
+     * @return
+     */
+    @ApiOperation("查询统计数据")
+    @PostMapping("/demandPlanSum/statisticsInfo")
+    AjaxResult statisticsInfo(@RequestBody DpDemandPlanSum queryCondition);
+
     /**
      * 导出需求计划汇总列表
-    */
+     */
     @ApiOperation("导出列表")
     @PostMapping("/demandPlanSum/exportData/{fileName}")
     byte[] exportData(@RequestBody DpDemandPlanSum queryVO, @PathVariable("fileName") String fileName);
+
     /**
      * 保存
      */
     @ApiOperation("保存")
     @PostMapping("/demandPlanSum/save")
     AjaxResult save(@RequestBody DpDemandPlanSum dpDemandPlanSum);
+
     /**
      * 查询需求计划版本号
      */
