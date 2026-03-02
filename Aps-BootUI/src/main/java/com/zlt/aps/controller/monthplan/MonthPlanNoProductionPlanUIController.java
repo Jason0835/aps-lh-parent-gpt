33c22128147ace929d1e024fcca6b737ad7de2eb
Abdul.Wu <wushuigen312@163.com>
2026-03-02 15:27:40 +0800

    未排计划排序调整

--- Aps-BootUI/src/main/java/com/zlt/aps/controller/monthplan/MonthPlanNoProductionPlanUIController.java
+++ Aps-BootUI/src/main/java/com/zlt/aps/controller/monthplan/MonthPlanNoProductionPlanUIController.java
@@ -53,8 +53,6 @@ public class MonthPlanNoProductionPlanUIController extends BaseUIController<Mont
     @PostMapping("/list")
     @ResponseBody
     public TableDataInfo list(MonthPlanNoProductionPlan monthPlanNoProductionPlan) {
-        String orderByInfo = "STRUCTURE_NAME,SPECIFICATIONS,MAIN_PATTERN,PATTERN,MAIN_MATERIAL_DESC,PRO_SIZE,UPDATE_TIME DESC";
-        startPage(orderByInfo);
         return iMonthPlanNoProductionPlanService.list(monthPlanNoProductionPlan);
     }
 
