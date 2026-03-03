acb201bef58fd4756a10904a2b0e1189a78e44b7
kf_hak <KF_Hak@zlt.com>
2026-03-03 09:24:31 +0800

    周程滚动搭配量需要扣减掉已搭配量

--- Aps-Engine/aps-engine-mp-core/src/main/java/com/zlt/aps/mp/engine/scheduling/matching/MatchingProductionHandler.java
+++ Aps-Engine/aps-engine-mp-core/src/main/java/com/zlt/aps/mp/engine/scheduling/matching/MatchingProductionHandler.java
@@ -285,7 +285,9 @@ public class MatchingProductionHandler {
                     mpProdFinalList, needProductPlanList, mdmSkuConstructionRefMap, capacity); // 获取定稿计划
             boolean isNewPlan = plan.getBeginDay() == null;
             
-            int unAllocationQty = needProductPlanList.stream().mapToInt(DpDemandPlan::getConventionReserveQty).sum(); // 储备量
+            int conventionReserveQty = needProductPlanList.stream().mapToInt(DpDemandPlan::getConventionReserveQty).sum(); // 储备量
+            int conventionProductionQty = Optional.ofNullable(plan.getConventionProductionQty()).orElse(0); // 已搭配量
+            int unAllocationQty = conventionReserveQty - conventionProductionQty; // 剩余搭配量 = 储备量 - 已搭配量
             unAllocationQty = isSpecial? Math.min(unAllocationQty, unAllocatSpecStructureTotalQty): unAllocationQty; // 如果包含特殊材料，不能超过特殊材料的总数量
             
             out: do {
