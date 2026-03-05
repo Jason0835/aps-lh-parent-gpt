500f8ff4228af1bc75ee73d669c43eb97193b1cb
kf_steve <KF_Steve@zlt.com>
2026-03-05 18:40:51 +0800

    20802 【SKU与示方关系】增加显示胎胚描述查询

--- APS-Modules/aps-mp/src/main/java/com/zlt/aps/mp/setting/controller/MdmSkuConstructionRefController.java
+++ APS-Modules/aps-mp/src/main/java/com/zlt/aps/mp/setting/controller/MdmSkuConstructionRefController.java
@@ -159,6 +159,7 @@ public class MdmSkuConstructionRefController extends AbstractDocBizController<Md
         queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("hydraulicPressureCuringTime")), "HYDRAULIC_PRESSURE_CURING_TIME", queryVO.getFieldValueByFieldName("hydraulicPressureCuringTime"));
         queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("curingTime2")), "CURING_TIME2", queryVO.getFieldValueByFieldName("curingTime2"));
         queryWrapper.eq(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("hydraulicPressureCuringTime2")), "HYDRAULIC_PRESSURE_CURING_TIME2", queryVO.getFieldValueByFieldName("hydraulicPressureCuringTime2"));
+        queryWrapper.like(PubUtil.isNotEmpty(queryVO.getFieldValueByFieldName("mainMaterialDesc")), "MAIN_MATERIAL_DESC", queryVO.getFieldValueByFieldName("mainMaterialDesc"));
     }
 
 
