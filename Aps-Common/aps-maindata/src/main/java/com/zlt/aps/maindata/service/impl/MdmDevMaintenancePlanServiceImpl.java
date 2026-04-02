ba16bb0aebe12870ebc0c3b7167805401e6e2b51
huangsencai <2771457033@qq.com>
2026-04-02 15:08:17 +0800

    规范修复service继承新框架IDocService的问题

--- Aps-Common/aps-maindata/src/main/java/com/zlt/aps/maindata/service/impl/MdmDevMaintenancePlanServiceImpl.java
+++ Aps-Common/aps-maindata/src/main/java/com/zlt/aps/maindata/service/impl/MdmDevMaintenancePlanServiceImpl.java
@@ -1,15 +1,25 @@
 package com.zlt.aps.maindata.service.impl;
 
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zlt.aps.maindata.mapper.MdmDevMaintenancePlanEntityMapper;
 import com.zlt.aps.maindata.service.IMdmDevMaintenancePlanService;
 import com.zlt.aps.mp.api.domain.entity.MdmDevMaintenancePlan;
+import com.zlt.bill.common.service.AbstractDocService;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * APS设备保养计划Service实现类
  */
 @Service
-public class MdmDevMaintenancePlanServiceImpl extends ServiceImpl<MdmDevMaintenancePlanEntityMapper, MdmDevMaintenancePlan> implements IMdmDevMaintenancePlanService {
+public class MdmDevMaintenancePlanServiceImpl extends AbstractDocService<MdmDevMaintenancePlan> implements IMdmDevMaintenancePlanService {
+
+    @Resource
+    private MdmDevMaintenancePlanEntityMapper mdmDevMaintenancePlanEntityMapper;
+
+    @Override
+    protected String getDocTypeCode() {
+        return "0";
+    }
 
 }
