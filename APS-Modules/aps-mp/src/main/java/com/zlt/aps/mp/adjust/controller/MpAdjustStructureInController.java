d4562399cceda74fe653515834bc488b5053fb3a
wengpc <wengpc@163.com>
2026-04-10 17:02:33 +0800

    21460 【月计划调整】确认调整量输入下 页面就会加载中

--- APS-Modules/aps-mp/src/main/java/com/zlt/aps/mp/adjust/controller/MpAdjustStructureInController.java
+++ APS-Modules/aps-mp/src/main/java/com/zlt/aps/mp/adjust/controller/MpAdjustStructureInController.java
@@ -65,7 +65,7 @@ public class MpAdjustStructureInController extends AbstractDocBizController<MpAd
     @PostMapping("/list")
     @Override
     public TableDataInfo list(@RequestBody MpAdjustStructureIn queryVO) {
-//        PageUtils.clearPage();
+        PageUtils.clearPage();
         TableDataInfo tableDataInfo = super.list(queryVO);
         sortlList(tableDataInfo.getRows());
         return tableDataInfo;
@@ -116,6 +116,11 @@ public class MpAdjustStructureInController extends AbstractDocBizController<MpAd
 
     }
 
+
+    @Override
+    protected String getOrderBy() {
+        return null;
+    }
 
     /**
      * 保存
