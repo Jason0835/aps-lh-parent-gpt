9d7218d9726da87b9e7351ecf98b89c159c842c6
huangsencai <2771457033@qq.com>
2026-04-13 09:37:06 +0800

    新增模具清洗预警和固化精度计划功能
    - 新增模具清洗预警API和页面
    - 新增固化精度计划API和页面
    - 更新模具清洗计划页面组件
    - 更新精度计划页面组件
    - 更新中文语言文件

---
+++ src/views/lh/mouldCleanWarn/index.vue
@@ -0,0 +1,190 @@
+<template>
+  <basic-container>
+    <page-table
+      tableRef="mouldCleanWarnMainTable"
+      :calcHeight="true"
+      v-loading="loading"
+      :columns="columns"
+      :searchColumns="searchColumns"
+      :data="data"
+      :page="page"
+      :search="search"
+      @refresh="getList"
+      @search="handleSearch"
+      @pageChange="handlePageChange"
+      @sort-change="handleSortChange"
+      @selection-change="handleSelectionChange"
+      :showSummary="false"
+      :selectArea="false"
+    >
+      <template slot="header">
+        <el-button
+          v-hasPermi="['lh:mouldCleanWarn:export']"
+          @click="handleExport"
+          >{{ $t("ui.frame.btn.export") }}</el-button
+        >
+      </template>
+    </page-table>
+  </basic-container>
+</template>
+
+<script>
+import { listMouldCleanWarn } from "@/api/lh/mouldCleanWarn";
+import { downloadLink } from "@/utils/ruoyi";
+
+export default {
+  name: "LhMouldCleanWarn",
+  dicts: ["biz_factory_name", "lh_machine"],
+  provide() {
+    return {
+      parentDict: this.dict,
+    };
+  },
+  data() {
+    return {
+      loading: false,
+      data: [],
+      selection: [],
+      page: {
+        current: 1,
+        pageSize: 20,
+        total: 0,
+      },
+      sort: {},
+      search: {},
+      query: {},
+    };
+  },
+  computed: {
+    columns() {
+      let columns = [
+        { type: "selection", fixed: "left" },
+        {
+          prop: "factoryCode",
+          label: this.$t("common.factory"),
+          formatter: (row, column, value) => {
+            return this.selectDictLabel(this.dict.type.biz_factory_name, value);
+          },
+        },
+        {
+          prop: "lhCode",
+          label: this.$t("ui.data.column.mouldCleanWarn.lhCode"),
+          width: 180
+        },
+        {
+          prop: "operTime",
+          label: this.$t("ui.data.column.mouldCleanWarn.operTime"),
+        },
+        {
+          prop: "firstWashTime",
+          label: this.$t("ui.data.column.mouldCleanWarn.firstWashTime"),
+        },
+        {
+          prop: "secondWashTime",
+          label: this.$t("ui.data.column.mouldCleanWarn.secondWashTime"),
+        },
+        {
+          prop: "remark",
+          label: this.$t("ui.data.column.mouldCleanWarn.remark"),
+          showOverflowTooltip: true
+        },
+        {
+          prop: "createTime",
+          label: this.$t("ui.data.column.mouldCleanWarn.createTime"),
+        },
+      ];
+
+      return columns;
+    },
+    searchColumns() {
+      return [
+        {
+          label: this.$t("ui.data.column.mouldCleanWarn.lhCode"),
+          prop: "lhCode",
+          type: "select",
+          dictData: this.dict.type.lh_machine,
+          filterable: true,
+        },
+        {
+          label: this.$t("ui.data.column.mouldCleanWarn.operTime"),
+          prop: "operTime",
+          type: "date",
+          dateType: "daterange",
+          valueFormat: "yyyy-MM-dd",
+        },
+      ];
+    },
+  },
+  methods: {
+    handleSearch(data) {
+      this.query = { ...data };
+      if (data.operTime && data.operTime.length === 2) {
+        this.query.operTimeBegin = data.operTime[0];
+        this.query.operTimeEnd = data.operTime[1];
+        delete this.query.operTime;
+      }
+      this.$set(this.page, "current", 1);
+      this.getList();
+    },
+    handlePageChange(current, pageSize) {
+      this.$set(this.page, "current", current);
+      this.$set(this.page, "pageSize", pageSize);
+      this.getList();
+    },
+    handleSortChange({ column, prop, order }) {
+      if (order) {
+        this.sort = {
+          orderByColumn: prop,
+          isAsc: order == "ascending" ? "asc" : "desc",
+        };
+      } else {
+        this.sort = {};
+      }
+      this.getList();
+    },
+    handleSelectionChange(rows) {
+      this.selection = rows;
+    },
+    handleExport() {
+      downloadLink("/lh/mouldCleanWarn/export", this.formatParams(false));
+    },
+    formatParams(hasPage = true) {
+      const params = {
+        ...this.query,
+        ...this.sort,
+      };
+
+      if (hasPage) {
+        params.pageSize = this.page.pageSize;
+        params.pageNum = this.page.current;
+      }
+
+      return params;
+    },
+    async getList() {
+      try {
+        this.loading = true;
+        const params = this.formatParams();
+        const res = await listMouldCleanWarn(params);
+        this.data = res.rows || [];
+        this.page.total = res.total || 0;
+      } catch (error) {
+        console.error(error);
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+  mounted() {
+    this.getList();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.mould-clean-warn {
+  height: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+}
+</style>
