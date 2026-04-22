d214cc70406974907f07788b6d183f388e510314
wengpc <wengpc@163.com>
2026-04-22 18:53:53 +0800

    21810 硫化排程管理---->做编辑操作，接口报错

--- APS-Modules/aps-lh/src/main/java/com/zlt/aps/lh/service/impl/LhScheduleServiceImpl.java
+++ APS-Modules/aps-lh/src/main/java/com/zlt/aps/lh/service/impl/LhScheduleServiceImpl.java
@@ -22,6 +22,7 @@ import com.zlt.aps.lh.mapper.LhScheduleResultMapper;
 import com.zlt.aps.lh.service.ILhScheduleService;
 import com.zlt.aps.lh.util.LhScheduleTimeUtil;
 import com.zlt.bill.common.service.AbstractDocService;
+import com.zlt.sysdef.domain.SysDocType;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
@@ -147,6 +148,15 @@ public class LhScheduleServiceImpl extends AbstractDocService<LhScheduleResult> 
     public String getDocTypeCode() {
         return "OUT2046";
     }
+
+
+    @Override
+    protected SysDocType getSysDocType() {
+        SysDocType sysDocType = new SysDocType();
+        sysDocType.setDocTypeCode("OUT2046");
+        return sysDocType;
+    }
+
 
     @Override
     public List<LhScheduleShiftDateVO> listScheduleShiftDates(Date scheduleDate) {
