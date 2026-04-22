0aed349d13015f144b46385e8f022308e172b4fc
kf_nick <kf_nick@qq.com>
2026-04-22 16:10:13 +0800

    成型算法更新 Nick

--- APS-Modules/aps-cx/src/main/java/com/zlt/aps/cx/service/engine/TaskGroupService.java
+++ APS-Modules/aps-cx/src/main/java/com/zlt/aps/cx/service/engine/TaskGroupService.java
@@ -342,9 +342,6 @@ public class TaskGroupService {
             totalFormingRemainder = formingRemainderMap.get(materialCode);
         }
 
-        // 获取当前任务分配的库存
-        int currentTaskStock = task.getCurrentStock() != null ? task.getCurrentStock() : 0;
-
         // 计算当前任务的剩余成型余量 = 总成型余量 - 已使用成型余量
         Integer remainingFormingRemainder = null;
         if (totalFormingRemainder != null) {
