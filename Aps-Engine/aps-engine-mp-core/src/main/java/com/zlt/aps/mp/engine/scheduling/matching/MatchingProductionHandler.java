80359c30011fd51f579eceb8f080c164f0890587
kf_hak <KF_Hak@zlt.com>
2026-04-22 16:52:13 +0700

    修复搭配排产后totalqty和每天汇总值可能不一致的问题

--- Aps-Engine/aps-engine-mp-core/src/main/java/com/zlt/aps/mp/engine/scheduling/matching/MatchingProductionHandler.java
+++ Aps-Engine/aps-engine-mp-core/src/main/java/com/zlt/aps/mp/engine/scheduling/matching/MatchingProductionHandler.java
@@ -2079,7 +2079,6 @@ public class MatchingProductionHandler {
                     plan.setPostponeQty(requirePlan.getPostponeQty());
                 }
                 plan.setConventionProductionQty(matchingQty);
-                plan.setTotalQty(oldPlan.getTotalQty() + matchingQty + factProdQty);
                 // 差异
                 plan.setDifferenceQty(oldPlan.getDifferenceQty() > factProdQty? oldPlan.getDifferenceQty() - factProdQty: 0);
                 plan.setMouldCavityQty(oldPlan.getMouldCavityQty());
@@ -2089,7 +2088,6 @@ public class MatchingProductionHandler {
                 plan.setId(oldPlan.getId());
             } else {
                 plan.setConventionProductionQty(matchingQty);
-                plan.setTotalQty(matchingQty + factProdQty);
                 if (plan.getProductionSequence() == null) {
                     productionSequence++;
                     plan.setProductionSequence(productionSequence);
@@ -2098,7 +2096,8 @@ public class MatchingProductionHandler {
                 plan.setFactProdReqQty(0);
                 plan.setReason(null);
             }
-            plan.allocateProductionByPriority();
+            plan.statisticsTotalQty(); // 重新统计汇总值
+            plan.allocateProductionByPriority(); // 按优先级重新分配实际排产量
             plan.setCreateBy(firstPlan.getCreateBy());
             plan.setCreateTime(firstPlan.getCreateTime());
             plan.setUpdateBy(firstPlan.getUpdateBy());
