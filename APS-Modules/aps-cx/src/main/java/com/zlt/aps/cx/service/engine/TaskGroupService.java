d86f213be6c08c343492afe5ca6a0b24fe9bfdb3
kf_nick <kf_nick@qq.com>
2026-04-27 17:33:12 +0800

    成型算法更新 Nick+

--- APS-Modules/aps-cx/src/main/java/com/zlt/aps/cx/service/engine/TaskGroupService.java
+++ APS-Modules/aps-cx/src/main/java/com/zlt/aps/cx/service/engine/TaskGroupService.java
@@ -200,8 +200,7 @@ public class TaskGroupService {
                 String factoryCode = context.getFactoryCode();
                 ScheduleDayTypeHelper.ShiftType st = scheduleDayTypeHelper.determineShiftType(
                         currentScheduleDate, currentShift.getDayShiftOrder(), factoryCode);
-                isOpeningShift = st == ScheduleDayTypeHelper.ShiftType.OPEN_START
-                        || scheduleDayTypeHelper.isOpeningDay(currentScheduleDate, factoryCode);
+                isOpeningShift = st == ScheduleDayTypeHelper.ShiftType.OPEN_START;
             }
         }
 
@@ -1147,7 +1146,7 @@ public class TaskGroupService {
      * <ol>
      *   <li>已停产日（isStopDay）→ 产量=0</li>
      *   <li>当前班次停产/停产前一个班次/停产标识日 → handleClosingDayTaskV2（反推封顶）</li>
-     *   <li>开产班次（OPEN_START 或 isOpeningDay）→ handleOpeningDayTaskV2（6/24备货）</li>
+     *   <li>开产班次（OPEN_START）→ handleOpeningDayTaskV2（6/24备货）</li>
      *   <li>明天有停产 → 跨天封顶</li>
      * </ol>
      *
