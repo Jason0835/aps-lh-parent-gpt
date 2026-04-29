91c9180131de8d40231f551bed1a5576748949a2
huangsencai <2771457033@qq.com>
2026-04-29 15:46:47 +0800

    漏提

---
+++ Aps-Api/aps-lh-api/src/main/java/com/zlt/aps/lh/api/domain/dto/LhInsertOrderValidateResultDTO.java
@@ -0,0 +1,111 @@
+package com.zlt.aps.lh.api.domain.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 硫化插单校验结果DTO
+ * <p>包含阻断性错误和非阻断性提示信息</p>
+ *
+ * @author APS
+ */
+@Data
+public class LhInsertOrderValidateResultDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 校验是否通过（无阻断性错误）
+     */
+    @ApiModelProperty(value = "校验是否通过", name = "valid")
+    private boolean valid;
+
+    /**
+     * 阻断性错误信息列表
+     */
+    @ApiModelProperty(value = "阻断性错误信息列表", name = "errorMessages")
+    private List<String> errorMessages = new ArrayList<>();
+
+    /**
+     * 提示性信息列表（非阻断，如产能提示、超产提示、模具可用提示）
+     */
+    @ApiModelProperty(value = "提示性信息列表", name = "warningMessages")
+    private List<String> warningMessages = new ArrayList<>();
+
+    /**
+     * 机台班产
+     */
+    @ApiModelProperty(value = "机台班产", name = "machineShiftCapacity")
+    private Integer machineShiftCapacity;
+
+    /**
+     * 各班次剩余产能（key: 班次索引1-8, value: 剩余产能）
+     */
+    @ApiModelProperty(value = "各班次剩余产能", name = "remainingCapacityByShift")
+    private List<ShiftCapacityInfo> remainingCapacityByShift = new ArrayList<>();
+
+    /**
+     * 硫化余量
+     */
+    @ApiModelProperty(value = "硫化余量", name = "mouldSurplusQty")
+    private Integer mouldSurplusQty;
+
+    /**
+     * 班次产能信息
+     */
+    @Data
+    public static class ShiftCapacityInfo implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        @ApiModelProperty(value = "班次索引", name = "shiftIndex")
+        private Integer shiftIndex;
+
+        @ApiModelProperty(value = "班次名称", name = "shiftName")
+        private String shiftName;
+
+        @ApiModelProperty(value = "机台班产", name = "shiftCapacity")
+        private Integer shiftCapacity;
+
+        @ApiModelProperty(value = "已排计划量", name = "scheduledQty")
+        private Integer scheduledQty;
+
+        @ApiModelProperty(value = "剩余产能", name = "remainingCapacity")
+        private Integer remainingCapacity;
+
+        public ShiftCapacityInfo() {
+        }
+
+        public ShiftCapacityInfo(Integer shiftIndex, String shiftName, Integer shiftCapacity,
+                                 Integer scheduledQty, Integer remainingCapacity) {
+            this.shiftIndex = shiftIndex;
+            this.shiftName = shiftName;
+            this.shiftCapacity = shiftCapacity;
+            this.scheduledQty = scheduledQty;
+            this.remainingCapacity = remainingCapacity;
+        }
+    }
+
+    /**
+     * 添加阻断性错误
+     *
+     * @param error 错误信息
+     */
+    public void addError(String error) {
+        this.errorMessages.add(error);
+        this.valid = false;
+    }
+
+    /**
+     * 添加提示性信息
+     *
+     * @param warning 提示信息
+     */
+    public void addWarning(String warning) {
+        this.warningMessages.add(warning);
+    }
+}
