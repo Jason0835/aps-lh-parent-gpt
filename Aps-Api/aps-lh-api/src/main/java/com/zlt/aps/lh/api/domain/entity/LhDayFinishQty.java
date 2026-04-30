5a2a5bb597f0b12b96f43df34e7b1a5f3267a46f
kf_steve <KF_Steve@zlt.com>
2026-04-30 17:56:21 +0800

    22035 需要加硫化日硫化日完成量的菜单和页面，只要查询功能，页面列表栏位，工厂，物料编码，完成量，完成日期，备注，更新时间

--- Aps-Api/aps-lh-api/src/main/java/com/zlt/aps/lh/api/domain/entity/LhDayFinishQty.java
+++ Aps-Api/aps-lh-api/src/main/java/com/zlt/aps/lh/api/domain/entity/LhDayFinishQty.java
@@ -2,6 +2,8 @@ package com.zlt.aps.lh.api.domain.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ruoyi.common.core.annotation.Excel;
 import com.ruoyi.common.core.web.domain.BaseEntity;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -26,31 +28,44 @@ public class LhDayFinishQty extends BaseEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Excel(name = "ui.data.column.lhDayFinishQty.finishDate", width = 30, dateFormat = "yyyy-MM-dd")
     @ApiModelProperty(value = "完成日期")
     @TableField(value = "FINISH_DATE")
     private Date finishDate;
 
+    @Excel(name = "ui.data.column.lhDayFinishQty.dayFinishQty")
     @ApiModelProperty(value = "胚胎日完成量")
     @TableField(value = "DAY_FINISH_QTY")
     private BigDecimal dayFinishQty;
 
+    @Excel(name = "ui.data.column.lhDayFinishQty.materialCode")
     @ApiModelProperty(value = "物料编码（NC）")
     @TableField(value = "MATERIAL_CODE")
     private String materialCode;
 
+    @Excel(name = "ui.data.column.lhDayFinishQty.mesMaterialCode")
     @ApiModelProperty(value = "物料编码（MES）")
     @TableField(value = "MES_MATERIAL_CODE")
     private String mesMaterialCode;
 
+    @Excel(name = "ui.data.column.lhDayFinishQty.dataVersion")
     @ApiModelProperty(value = "版本号")
     @TableField(value = "DATA_VERSION")
     private String dataVersion;
 
+    @Excel(name = "ui.data.column.lhDayFinishQty.companyCode")
     @ApiModelProperty(value = "分公司编码")
     @TableField(value = "COMPANY_CODE")
     private String companyCode;
 
+    @Excel(name = "ui.data.column.lhDayFinishQty.factoryCode")
     @ApiModelProperty(value = "厂别")
     @TableField(value = "FACTORY_CODE")
     private String factoryCode;
+
+    @Excel(name = "ui.data.column.lhDayFinishQty.materialDesc")
+    @ApiModelProperty(value = "物料描述")
+    @TableField(exist = false)
+    private String materialDesc;
 }
