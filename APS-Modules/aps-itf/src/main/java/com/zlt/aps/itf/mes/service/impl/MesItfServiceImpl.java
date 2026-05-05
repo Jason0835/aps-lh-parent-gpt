3899f6025518c9b27987e670122f70af5d3b8ebf
kf_hak <KF_Hak@zlt.com>
2026-05-05 09:50:35 +0700

    硫化日计划同步接口，如果传参没有版本号则自动改为同步上一天数据

--- APS-Modules/aps-itf/src/main/java/com/zlt/aps/itf/mes/service/impl/MesItfServiceImpl.java
+++ APS-Modules/aps-itf/src/main/java/com/zlt/aps/itf/mes/service/impl/MesItfServiceImpl.java
@@ -1591,6 +1591,11 @@ public class MesItfServiceImpl implements MesItfService {
     @Override
     public AjaxResult syncLhScheDayFinishQty(AuxReqSyncDataLogs syncDataLogs) {
         DynamicDataSourceContextHolder.push(DataSource.MES);
+        if (StringUtils.isEmpty(syncDataLogs.getDataVersion())) { // 如果没有数据版本，则取完成日期为上一天的数据
+            syncDataLogs.setQueryParams(new HashMap<>());
+            Date nowDate = DateUtils.truncate(DateUtils.getNowDate(), Calendar.DATE); // 当日0点
+            syncDataLogs.getQueryParams().put("finishDate", DateUtils.addDays(nowDate, -1)); // 取上一天的日期
+        }
         List<LhDayFinishQty> syncList = mesItfMapper.selectLhScheDayFinishQtyList(syncDataLogs);
         DynamicDataSourceContextHolder.poll();
 
