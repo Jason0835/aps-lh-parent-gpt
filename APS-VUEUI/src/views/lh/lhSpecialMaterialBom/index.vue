def8e4284ecf10bd82a8b3cbc0867bf06e96dfe7
huangsencai <2771457033@qq.com>
2026-05-06 20:06:41 +0800

    1

---
+++ APS-VUEUI/src/views/lh/lhSpecialMaterialBom/index.vue
@@ -0,0 +1,328 @@
+<template>
+  <basic-container>
+    <page-table
+      tableRef="LhSpecialMaterialBomMainTable"
+      :calcHeight="true"
+      v-loading="loading"
+      :columns="columns"
+      :searchColumns="searchColumns"
+      :data="data"
+      :page="page"
+      :search="search"
+      @refresh="getList"
+      @search="handleSearch"
+      @pageChange="handlePageChange"
+      @sort-change="handleSortChange"
+      @selection-change="handleSelectionChange"
+      :showSummary="false"
+      :selectArea="false"
+    >
+      <template slot="header">
+        <el-button
+          type="primary"
+          v-hasPermi="['lh:lhSpecialMaterialBom:add']"
+          @click="handleAdd"
+        >{{ $t("ui.frame.btn.add") }}</el-button>
+        <el-button
+          v-hasPermi="['lh:lhSpecialMaterialBom:edit']"
+          @click="handleBatchEdit"
+          :disabled="selection.length !== 1"
+        >{{ $t("ui.frame.btn.update") }}</el-button>
+        <el-button
+          type="danger"
+          v-hasPermi="['lh:lhSpecialMaterialBom:remove']"
+          :disabled="selection.length == 0"
+          @click="handleBatchDelete"
+        >{{ $t("ui.frame.btn.delete") }}</el-button>
+        <el-button
+          v-hasPermi="['lh:lhSpecialMaterialBom:import']"
+          @click="$refs.tltUpload.handleImport()"
+        >{{ $t("ui.frame.btn.import") }}</el-button>
+        <el-button
+          @click="handleExport"
+          v-hasPermi="['lh:lhSpecialMaterialBom:export']"
+        >{{ $t("ui.frame.btn.export") }}</el-button>
+      </template>
+    </page-table>
+    <tlt-upload-form
+      ref="tltUpload"
+      :updateSupport="true"
+      downloadUrl="/lh/lhSpecialMaterialBom/importTemplate"
+      uploadUrl="/lh/lhSpecialMaterialBom/importData"
+      @uploadSuccess="getList"
+      labelWidth="0"
+      :columns="importColumns"
+    ></tlt-upload-form>
+    <infoDialog ref="infoRef" @success="getList" />
+  </basic-container>
+</template>
+<script>
+import { downloadLink } from "@/utils/request";
+import { listSpecialMaterialBom, removeSpecialMaterialBom } from "@/api/lh/specialMaterialBom";
+import TltUploadForm from "@/views/components/tltUploadForm.vue";
+import infoDialog from "./components/infoDialog.vue";
+
+export default {
+  name: "LhSpecialMaterialBom",
+  components: {
+    TltUploadForm,
+    infoDialog,
+  },
+  dicts: ["biz_factory_name", "lh_special_material_category"],
+  provide() {
+    return {
+      parentDict: this.dict,
+    };
+  },
+  data() {
+    return {
+      importColumns: [
+        {
+          label: "",
+          prop: "updateSupport",
+          render: (form) => {
+            return (
+              <el-checkbox
+                label={this.$t("common.rule.updateSupport")}
+                v-model={form.updateSupport}
+              >
+                {this.$t("common.rule.updateSupport")}
+              </el-checkbox>
+            );
+          },
+        },
+      ],
+      loading: false,
+      data: [],
+      selection: [],
+      page: {
+        current: 1,
+        pageSize: 20,
+        total: 0,
+      },
+      sort: {},
+      search: {
+        factoryCode: "116",
+      },
+      query: {
+        factoryCode: "116",
+      },
+    };
+  },
+  computed: {
+    columns() {
+      let columns = [
+        { type: "selection", fixed: "left" },
+        {
+          prop: "factoryCode",
+          align: "center",
+          halign: "center",
+          label: this.$t("ui.data.column.factoryCode"),
+          minWidth: 120,
+          formatter: (row, column, value, index) => {
+            return this.selectDictLabel(this.dict.type.biz_factory_name, value);
+          },
+        },
+        {
+          prop: "structureName",
+          align: "center",
+          halign: "center",
+          label: this.$t("ui.data.column.lhSpecialMaterialBom.structureName"),
+          minWidth: 200,
+        },
+        {
+          prop: "materialCode",
+          align: "center",
+          halign: "center",
+          label: this.$t("ui.data.column.lhSpecialMaterialBom.materialCode"),
+          minWidth: 150,
+        },
+        {
+          prop: "materialDesc",
+          align: "left",
+          halign: "center",
+          label: this.$t("ui.data.column.lhSpecialMaterialBom.materialDesc"),
+          minWidth: 250,
+        },
+        {
+          prop: "category",
+          align: "center",
+          halign: "center",
+          label: this.$t("ui.data.column.lhSpecialMaterialBom.category"),
+          minWidth: 120,
+          formatter: (row, column, value, index) => {
+            return this.selectDictLabel(this.dict.type.lh_special_material_category, value);
+          },
+        },
+        {
+          prop: "updateTime",
+          align: "center",
+          halign: "center",
+          label: this.$t("ui.data.column.updateTime"),
+          minWidth: 160,
+        },
+        {
+          align: "center",
+          halign: "center",
+          label: this.$t("ui.data.btn.option"),
+          prop: "option",
+          minWidth: 150,
+          render: ({ row }) => {
+            return (
+              <div>
+                <el-button
+                  v-hasPermi={["lh:lhSpecialMaterialBom:edit"]}
+                  class="minus"
+                  type="success"
+                  onClick={() => this.handleEdit(row)}
+                >
+                  {this.$t("ui.frame.btn.update")}
+                </el-button>
+                <el-button
+                  v-hasPermi={["lh:lhSpecialMaterialBom:remove"]}
+                  class="minus"
+                  type="danger"
+                  onClick={() => this.handleDelete(row)}
+                >
+                  {this.$t("ui.frame.btn.delete")}
+                </el-button>
+              </div>
+            );
+          },
+        },
+      ];
+      return columns;
+    },
+    searchColumns() {
+      return [
+        {
+          label: this.$t("ui.data.column.factoryCode"),
+          prop: "factoryCode",
+          type: "select",
+          dictData: this.dict.type.biz_factory_name,
+          filterable: true,
+        },
+        {
+          label: this.$t("ui.data.column.lhSpecialMaterialBom.structureName"),
+          prop: "structureName",
+        },
+        {
+          label: this.$t("ui.data.column.lhSpecialMaterialBom.materialCode"),
+          prop: "materialCode",
+        },
+        {
+          label: this.$t("ui.data.column.lhSpecialMaterialBom.materialDesc"),
+          prop: "materialDesc",
+        },
+        {
+          label: this.$t("ui.data.column.lhSpecialMaterialBom.category"),
+          prop: "category",
+          type: "select",
+          dictData: this.dict.type.lh_special_material_category,
+          filterable: true,
+        },
+      ];
+    },
+  },
+  methods: {
+    handleAdd() {
+      if (this.$refs.infoRef) {
+        this.$refs.infoRef.show();
+      }
+    },
+    handleEdit(row) {
+      if (this.$refs.infoRef) {
+        this.$refs.infoRef.show(row);
+      }
+    },
+    handleDelete(row) {
+      this.$confirm(this.$t("common.confirm.delete"), {
+        type: "warning",
+      }).then(() => {
+        const ids = row.id;
+        removeSpecialMaterialBom({ ids }).then((data) => {
+          this.$modal.msgSuccess(data.msg);
+          this.$set(this.page, "current", 1);
+          this.getList();
+        });
+      });
+    },
+    handleBatchEdit() {
+      if (this.selection && this.selection.length === 1) {
+        this.handleEdit(this.selection[0]);
+      }
+    },
+    handleBatchDelete() {
+      if (!this.selection || this.selection.length === 0) {
+        return;
+      }
+      this.$confirm(this.$t("common.confirm.delete"), {
+        type: "warning",
+      }).then(() => {
+        const ids = this.selection.map((item) => item.id).join(",");
+        removeSpecialMaterialBom({ ids }).then((data) => {
+          this.$modal.msgSuccess(data.msg);
+          this.selection = [];
+          this.$set(this.page, "current", 1);
+          this.getList();
+        });
+      });
+    },
+    handleSearch(data) {
+      this.query = { ...data };
+      this.$set(this.page, "current", 1);
+      this.getList();
+    },
+    handlePageChange(current, pageSize) {
+      this.$set(this.page, "current", current);
+      this.$set(this.page, "pageSize", pageSize);
+      this.getList();
+    },
+    handleSortChange({ column, prop, order }) {
+      if (order) {
+        this.sort = {
+          orderByColumn: prop,
+          isAsc: order == "ascending" ? "asc" : "desc",
+        };
+      } else {
+        this.sort = {};
+      }
+      this.getList();
+    },
+    handleSelectionChange(rows) {
+      this.selection = rows;
+    },
+    handleExport() {
+      downloadLink("/lh/lhSpecialMaterialBom/export", this.formatParams(false));
+    },
+    formatParams(hasPage = true) {
+      const params = {
+        ...this.query,
+        ...this.sort,
+      };
+      if (hasPage) {
+        params.pageSize = this.page.pageSize;
+        params.pageNum = this.page.current;
+      }
+      return params;
+    },
+    async getList() {
+      try {
+        this.loading = true;
+        const data = await listSpecialMaterialBom(this.formatParams());
+        this.data = data.rows;
+        this.page.total = data.total;
+      } catch (error) {
+        console.error(error);
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+  activated() {
+    this.getList();
+  },
+};
+</script>
+<style lang="scss" scoped>
+</style>
