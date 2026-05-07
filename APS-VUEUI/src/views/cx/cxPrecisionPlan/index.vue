89c5499a423a629bc950cd97018171873bd8770f
kf_show <zhangxuehuang@live.com>
2026-05-07 14:13:25 +0800

    修改硫化机位置

--- APS-VUEUI/src/views/cx/cxPrecisionPlan/index.vue
+++ APS-VUEUI/src/views/cx/cxPrecisionPlan/index.vue
@@ -120,6 +120,11 @@ export default {
           label: this.$t('ui.data.column.cxPrecisionPlan.cycle'),
           formatter: (row) => this.getCycleValue(row.precisionType)
         },
+        {
+          prop: 'cycle',
+          label: this.$t('ui.data.column.cxPrecisionPlan.cycle'),
+          formatter: (row) => this.getCycleValue(row.precisionType)
+        },
         { prop: 'planDate', label: this.$t('ui.data.column.cxPrecisionPlan.planDate') },
         { prop: 'actualDate', label: this.$t('ui.data.column.cxPrecisionPlan.actualDate') },
 
