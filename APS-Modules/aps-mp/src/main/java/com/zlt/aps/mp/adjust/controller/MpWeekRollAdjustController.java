fdc8ee90e4bbf9272f3fa7ffc46aeec4f7199b59
sandy <qazwsx@1008>
2026-05-09 19:21:19 +0700

    优化：调整排序增加同胎胚

--- APS-Modules/aps-mp/src/main/java/com/zlt/aps/mp/adjust/controller/MpWeekRollAdjustController.java
+++ APS-Modules/aps-mp/src/main/java/com/zlt/aps/mp/adjust/controller/MpWeekRollAdjustController.java
@@ -152,7 +152,7 @@ public class MpWeekRollAdjustController extends BaseController {
             weekAdjustStrategy.autoAdjust(contextDTO);
             log.info("自动调整 ==> 完成执行策略:[{}] 年月:[{}]", WeekAdjustTypeEnum.getByCode(weekRollAdjustDTO.getAdjustType()).getName(),
                     String.format("%d%02d", contextDTO.getMpYear(), contextDTO.getMpMonth()));
-            // 排序 按英寸->结构->最大型腔数->主花纹->活块数->物料描述
+            // 排序 按英寸->结构->同胎胚->最大型腔数->主花纹->活块数->物料描述
             sortAdjustResultList(contextDTO.getAdjustResultList());
             if (StringUtil.isEmptyWithTrim(contextDTO.getMsgStructureAdjustPreClose().toString())){
                 return AjaxResult.success(contextDTO.getMsgStructureAdjustPreClose().toString(),contextDTO.getAdjustResultList());
@@ -228,6 +228,8 @@ public class MpWeekRollAdjustController extends BaseController {
         // 一级排序：结构名称升序，空值排最后
         return Comparator.comparing(MpAdjustResult::getTbrProSize, Comparator.nullsLast(String::compareTo))
                 .thenComparing(MpAdjustResult::getStructureName,Comparator.nullsLast(String::compareTo))
+                // 同胎胚
+                .thenComparing(MpAdjustResult::getEmbryoCode,Comparator.nullsLast(String::compareTo))
                 // 最大型腔数
                 .thenComparing(MpAdjustResult::getMaxMouldCavityQty, Comparator.reverseOrder())
                 // 主花纹
