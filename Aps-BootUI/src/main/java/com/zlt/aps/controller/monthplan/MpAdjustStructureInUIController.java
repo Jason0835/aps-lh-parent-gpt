e85fea50d34524ea47e0221d97c34692934bdde3
kf_steve <KF_Steve@zlt.com>
2026-05-12 16:09:21 +0800

    月计划调整，多余权限去除

--- Aps-BootUI/src/main/java/com/zlt/aps/controller/monthplan/MpAdjustStructureInUIController.java
+++ Aps-BootUI/src/main/java/com/zlt/aps/controller/monthplan/MpAdjustStructureInUIController.java
@@ -1,7 +1,6 @@
 package com.zlt.aps.controller.monthplan;
 
-import cn.hutool.core.util.NumberUtil;
-import cn.hutool.core.util.StrUtil;
+import com.ruoyi.api.gateway.system.domain.vo.ImportContext;
 import com.ruoyi.common.core.utils.poi.ExcelUtil;
 import com.ruoyi.common.core.web.domain.AjaxResult;
 import com.ruoyi.common.core.web.page.TableDataInfo;
@@ -11,30 +10,22 @@ import com.ruoyi.common4ui.constant.UserConstants;
 import com.ruoyi.common4ui.core.controller.BaseUIController;
 import com.zlt.aps.mp.api.domain.entity.MpAdjustStructureIn;
 import com.zlt.aps.mp.api.service.IMpAdjustStructureInRemoteService;
+import com.zlt.file.encryptbyll.FileEncryptUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-
-import lombok.extern.slf4j.Slf4j;
-import com.ruoyi.api.gateway.system.domain.vo.ImportContext;
-import com.zlt.file.encryptbyll.FileEncryptUtils;
-import org.apache.commons.io.IOUtils;
-
-import java.util.Arrays;
-import java.io.IOException;
-import java.io.ByteArrayInputStream;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Copyright (c) 2022, All rights reserved。
@@ -91,7 +82,7 @@ public class MpAdjustStructureInUIController extends BaseUIController<MpAdjustSt
      * 根据条件查询主表数据
      */
     @ApiOperation("根据条件查询主表数据")
-    @RequiresPermissions("monthplan:mpAdjustStructureIn:list")
+//    @RequiresPermissions("monthplan:mpAdjustStructureIn:list")
     @PostMapping("/list")
     @ResponseBody
     public TableDataInfo list(MpAdjustStructureIn mpAdjustStructureIn) {
@@ -123,7 +114,7 @@ public class MpAdjustStructureInUIController extends BaseUIController<MpAdjustSt
      * 删除调整-结构内调整记录
      */
     @ApiOperation("删除,id不为空")
-    @RequiresPermissions("monthplan:mpAdjustStructureIn:remove")
+//    @RequiresPermissions("monthplan:mpAdjustStructureIn:remove")
     @PostMapping("/remove")
     @ResponseBody
     public AjaxResult remove(String ids) {
