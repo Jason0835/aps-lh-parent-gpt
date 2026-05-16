5c974fe2b6137bb880a242cd0f1d16d447fa8368
kf_nick <kf_nick@qq.com>
2026-05-16 16:16:56 +0800

    成型算法更新 Nick+

--- APS-Modules/aps-cx/src/main/java/com/zlt/aps/cx/mapper/FactoryParamMapper.java
+++
@@ -1,9 +0,0 @@
-package com.zlt.aps.cx.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zlt.aps.mp.api.domain.entity.FactoryParam;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface FactoryParamMapper extends BaseMapper<FactoryParam> {
-}
