d8b60689e6656233adca3f2c478e3210cd0e0965
kf_hak <lwh@zlt.com>
2026-05-28 16:44:00 +0800

    修复月计划导出功能净需求数值计算不正确的问题

--- APS-Modules/aps-mp/src/main/java/com/zlt/aps/mp/factory/service/impl/MpStructureAllocationServiceImpl.java
+++ APS-Modules/aps-mp/src/main/java/com/zlt/aps/mp/factory/service/impl/MpStructureAllocationServiceImpl.java
@@ -1245,13 +1245,12 @@ public class MpStructureAllocationServiceImpl extends AbstractDocService<MpStruc
         // 1.5、加载需求计划
         QueryWrapper<DpDemandPlan> dpDemandPlanQueryWrapper = new QueryWrapper<>();
         dpDemandPlanQueryWrapper.select("STRUCTURE_NAME", "SUM(UN_POSTPONE_NET_QTY) UN_POSTPONE_NET_QTY",
-                "SUM(HEIGHT_QTY) HEIGHT_QTY");
+                "SUM(HEIGHT_QTY) HEIGHT_QTY", "SUM(ORI_HEIGHT_QTY) ORI_HEIGHT_QTY");
         dpDemandPlanQueryWrapper.groupBy("STRUCTURE_NAME");
         dpDemandPlanQueryWrapper.eq("FACTORY_CODE", param.getFactoryCode());
         dpDemandPlanQueryWrapper.eq("MONTH_PLAN_VERSION", param.getMonthPlanVersion());
         List<DpDemandPlan> dpDemandPlanList = dpDemandPlanEntityMapper.selectList(dpDemandPlanQueryWrapper);
         Map<String, DpDemandPlan> dpDemandPlanMap = dpDemandPlanList.stream()
-                .filter(p -> p.getUnPostponeNetQty() != null)
                 .collect(Collectors.toMap(DpDemandPlan::getStructureName, Function.identity()));
 
         // 2、构建报表头
@@ -1334,8 +1333,11 @@ public class MpStructureAllocationServiceImpl extends AbstractDocService<MpStruc
                 machineRecord.setChangeRank(changeRank ++); // 设置序号
                 machineRecord.setBeginDay(beginDay);
                 machineRecord.setEndDay(endDay);
-                machineRecord.setUnPostponeNetQty(Optional.ofNullable(dpDemandPlanMap.get(structureName)).map(DpDemandPlan::getUnPostponeNetQty).orElse(0));
-                machineRecord.setHeightQty(Optional.ofNullable(dpDemandPlanMap.get(structureName)).map(DpDemandPlan::getHeightQty).orElse(0));
+                DpDemandPlan dpDemandPlan = Optional.ofNullable(dpDemandPlanMap.get(structureName)).orElse(null);
+                if (dpDemandPlan != null) {
+                    machineRecord.setUnPostponeNetQty(Convert.toInt(dpDemandPlan.getUnPostponeNetQty(), 0));
+                    machineRecord.setHeightQty(Convert.toInt(dpDemandPlan.getOriHeightQty(), Convert.toInt(dpDemandPlan.getHeightQty(), 0)));
+                }
                 if (beginDay != null && endDay != null) {
                     machineRecord.setAllotDays(endDay - beginDay + 1);
                 }
