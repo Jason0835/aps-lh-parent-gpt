865edaa12aebdbb3baf8306e1fd95aa56628fe27
abdul <wushuigen312@163.com>
2026-06-04 16:32:10 +0700

    月计划排产导出预警标记处理

--- APS-Modules/aps-mp/src/main/java/com/zlt/aps/mp/enums/MonthPlanExportDataTypeEnum.java
+++ APS-Modules/aps-mp/src/main/java/com/zlt/aps/mp/enums/MonthPlanExportDataTypeEnum.java
@@ -5,18 +5,36 @@ import lombok.Getter;
 
 /**
  * 月计划导出数据类型枚举
- * @author hak
  *
+ * @author hak
  */
 @Getter
 @AllArgsConstructor
 public enum MonthPlanExportDataTypeEnum {
-    RECORD("1","ui.data.column.factoryMonthPlanMouldDayResult.export.dataType.record"), // 明细记录
-    EMBRYO_TYPE_COUNT("2","ui.data.column.factoryMonthPlanMouldDayResult.export.dataType.embryoTypeCount"), // 胎胚种类数
-    LH_MACHINES("3","ui.data.column.factoryMonthPlanMouldDayResult.export.dataType.lhMachines"), // 硫化机台数
-    SUBTOTAL("4","ui.data.column.factoryMonthPlanMouldDayResult.export.dataType.subtotal"), // 小计
-    TOTAL("5","ui.data.column.factoryMonthPlanMouldDayResult.export.dataType.total"), // 合计
-    CHANGE_MOULDS("6","ui.data.column.factoryMonthPlanMouldDayResult.export.dataType.changeMoulds"); // 换模次数
+    /**
+     * 1 明细记录
+     */
+    RECORD("1", "ui.data.column.factoryMonthPlanMouldDayResult.export.dataType.record"),
+    /**
+     * 2 胎胚种类数
+     */
+    EMBRYO_TYPE_COUNT("2", "ui.data.column.factoryMonthPlanMouldDayResult.export.dataType.embryoTypeCount"),
+    /**
+     * 3 硫化机台数
+     */
+    LH_MACHINES("3", "ui.data.column.factoryMonthPlanMouldDayResult.export.dataType.lhMachines"),
+    /**
+     * 4 小计
+     */
+    SUBTOTAL("4", "ui.data.column.factoryMonthPlanMouldDayResult.export.dataType.subtotal"),
+    /**
+     * 5 合计
+     */
+    TOTAL("5", "ui.data.column.factoryMonthPlanMouldDayResult.export.dataType.total"),
+    /**
+     * 6 换模次数
+     */
+    CHANGE_MOULDS("6", "ui.data.column.factoryMonthPlanMouldDayResult.export.dataType.changeMoulds");
     final String code;
     final String name;
 }
