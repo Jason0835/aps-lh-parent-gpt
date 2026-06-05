4a325a7d579f65a8e7017da642225eda7faad4be
kf_show <zhangxuehuang@live.com>
2026-06-05 09:56:49 +0800

    删除cd90api

--- APS-Modules/aps-cd90/src/main/java/com/zlt/aps/cd90/ApsCd90Application.java
+++ APS-Modules/aps-cd90/src/main/java/com/zlt/aps/cd90/ApsCd90Application.java
@@ -6,12 +6,14 @@ import com.ruoyi.common.swagger.annotation.EnableCustomSwagger2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @EnableCustomConfig
 @SpringBootApplication
 @EnableCustomSwagger2
 @EnableRyFeignClients
-@ComponentScan(value={"com.zlt.*"})
+@ComponentScan(value = {"com.zlt.*", "com.zlt.aps.**"})
 public class ApsCd90Application {
 
     public static void main(String[] args) {
