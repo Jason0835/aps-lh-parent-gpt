4552557de5c0dd06835cfa26387d1bc84ae24989
kf_hak <lwh@zlt.com>
2026-06-30 16:28:43 +0800

    修复月计划导入需求没有算损耗率、导出上月定稿换模次数统计不正确的问题

--- APS-Modules/aps-mp/src/main/java/com/zlt/aps/mp/factory/service/impl/MpStructureAllocationServiceImpl.java
+++ APS-Modules/aps-mp/src/main/java/com/zlt/aps/mp/factory/service/impl/MpStructureAllocationServiceImpl.java
@@ -3438,6 +3438,10 @@ public class MpStructureAllocationServiceImpl extends AbstractDocService<MpStruc
                 insertItem.setProdReqPlan(demandPlan.getNetQty());
                 insertItem.setStructureType(demandPlan.getStructureType());
                 insertItem.setMainPattern(demandPlan.getMainPattern());
+                Integer netQty = safeAdd(demandPlan.getHeightQty(), demandPlan.getMidQty(), demandPlan.getPostponeQty()); // 总需求
+                Integer lossQty = (netQty & 1) == 0?ProductionConstant.ADD_LOSS_QTY_EVEN_NUMBER : ProductionConstant.ADD_LOSS_QTY_ODD_NUMBER;
+                Integer factProdReqQty = netQty + lossQty; // 总需求+(奇数+3/偶数+2)
+                insertItem.setFactProdReqQty(factProdReqQty);
                 // 计算库销比
                 insertItem.setInventorySalesRatio(BigDecimalUtils.div(demandPlan.getStockQty(), demandPlan.getAverageSaleQty(), 1));
             } else { // 试产试制规格从物料信息表加载信息
