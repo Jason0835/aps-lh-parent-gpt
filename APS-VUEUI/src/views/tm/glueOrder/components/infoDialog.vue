4c40b27af7c5b0a453c06d406d1fabe3bb1de919
kf_steve <KF_Steve@zlt.com>
2026-07-02 19:16:08 +0800

    22759 【胎面机台生产速度管理】新增/编辑页面，工厂字段设置为不可选择，根据机台编码带出 胎面功能统一调整

--- APS-VUEUI/src/views/tm/glueOrder/components/infoDialog.vue
+++ APS-VUEUI/src/views/tm/glueOrder/components/infoDialog.vue
@@ -88,6 +88,7 @@ export default {
           type: "select",
           span: 12,
           required: true,
+          disabled: true,
           dictData: this.parentDict.type.biz_factory_name,
         },
         {
@@ -117,6 +118,16 @@ export default {
           dictData: this.machines,
           props: { label: "machineCode", value: "machineCode" },
           filterable: true,
+          listeners: {
+            change: (value) => {
+              if (value) {
+                const machine = this.machines.find(m => m.machineCode === value);
+                if (machine) {
+                  this.form.factoryCode = machine.factoryCode;
+                }
+              }
+            },
+          },
         },
         {
           prop: "orderNum",
