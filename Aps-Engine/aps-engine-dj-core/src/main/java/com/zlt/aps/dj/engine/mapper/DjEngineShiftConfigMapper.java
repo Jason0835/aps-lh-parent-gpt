4820453ac4fed13af1ace60a50f5652eb2456880
kf_hak <lwh@zlt.com>
2026-07-06 14:11:45 +0800

    增加获取垫胶班次配置mapper

---
+++ Aps-Engine/aps-engine-dj-core/src/main/java/com/zlt/aps/dj/engine/mapper/DjEngineShiftConfigMapper.java
@@ -0,0 +1,10 @@
+package com.zlt.aps.dj.engine.mapper;
+
+import com.zlt.aps.dj.api.domain.entity.DjShiftConfig;
+import com.zlt.core.dao.basemapper.CommBaseMapper;
+
+/**
+ * 班次配置 Mapper（引擎模块）
+ */
+public interface DjEngineShiftConfigMapper extends CommBaseMapper<DjShiftConfig> {
+}
