2074f52e7a1c2515d885f09d57c695ad98c5ba00
kf_hak <lwh@zlt.com>
2026-07-10 14:04:39 +0800

    垫胶自动排程增加排程日志记录

---
+++ Aps-Engine/aps-engine-dj-core/src/main/java/com/zlt/aps/dj/engine/mapper/DjEngineScheduleProcessLogMapper.java
@@ -0,0 +1,11 @@
+package com.zlt.aps.dj.engine.mapper;
+
+import com.zlt.aps.dj.api.domain.entity.DjScheduleProcessLog;
+import com.zlt.core.dao.basemapper.CommBaseMapper;
+
+/**
+ * 垫胶排程日志 Mapper
+ */
+public interface DjEngineScheduleProcessLogMapper extends CommBaseMapper<DjScheduleProcessLog> {
+
+}
