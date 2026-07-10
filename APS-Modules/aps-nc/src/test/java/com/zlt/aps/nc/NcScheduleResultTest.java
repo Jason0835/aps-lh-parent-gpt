b5c77a552e071593cec87ca061aed7bd2fb950f5
kf_hak <lwh@zlt.com>
2026-07-10 09:21:53 +0800

    注释单元测试，提高打包速度

--- APS-Modules/aps-nc/src/test/java/com/zlt/aps/nc/NcScheduleResultTest.java
+++ APS-Modules/aps-nc/src/test/java/com/zlt/aps/nc/NcScheduleResultTest.java
@@ -6,16 +6,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.zlt.aps.nc.api.domain.entity.NcStock;
+import com.zlt.aps.nc.controller.NcStockController;
 import com.zlt.aps.nc.engine.service.NcEngineService;
 
 @SpringBootTest
 class NcScheduleResultTest {
 	@Autowired
 	private NcEngineService ncEngineService;
+    @Autowired
+	private NcStockController ncStockController;
 	
 	@Test
 	public void test() throws IOException {
-	    ncEngineService.autoNcSchedule("2025-08-13");
+//	    ncEngineService.autoNcSchedule("2025-08-13");
+//	    NcStock stock = new NcStock();
+//	    stock.setFactoryCode("116");
+//	    ncStockController.list(stock);
 	}
 
 }
