780653ef173b8ac8b4b0a906f862ce26fcfea6ab
kf_show <zhangxuehuang@live.com>
2026-07-11 22:21:08 +0800

    fix: add cd15 stock view alias

---
+++ APS-VUEUI/src/views/cd15/cd15Stock/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Cut15Stock />
+</template>
+
+<script>
+import Cut15Stock from "@/views/cut15/stock/index.vue";
+
+export default {
+  name: "Cd15Stock",
+  components: {
+    Cut15Stock,
+  },
+};
+</script>
