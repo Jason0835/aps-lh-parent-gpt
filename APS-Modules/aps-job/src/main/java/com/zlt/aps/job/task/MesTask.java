d307272ed0daa5d46818166f23753141c5659489
huangsencai <2771457033@qq.com>
2026-07-14 15:05:52 +0800

    1、硫化排程日完成量回报抓取指定版本数据 2、计算月底超欠产调整获取库存抓取日逻辑

--- APS-Modules/aps-job/src/main/java/com/zlt/aps/job/task/MesTask.java
+++ APS-Modules/aps-job/src/main/java/com/zlt/aps/job/task/MesTask.java
@@ -411,7 +411,7 @@ public class MesTask {
      */
     @ApiOperation("临时任务-按最新版本号抓取硫化排程日完成量回报（不限日期）")
     public void syncLhScheDayFinishQtyByLatestVersion() {
-        String dataVersion = "APS_MES_AH01_20260709110000012";
+        String dataVersion = "APS_MES_AH01_20260714105900022";
         log.info("临时任务-开始按最新版本号抓取硫化排程日完成量回报（不限日期）");
         try {
             FeignTokenHelper.runWithToken(() -> {
