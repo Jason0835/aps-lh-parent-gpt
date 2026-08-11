aa99dc02a050f313b0a32645a786ec18c3a2bed9
kf_steve <KF_Steve@zlt.com>
2026-08-11 13:59:46 +0800

    导入必填校验注解调整

--- Aps-Api/tm-api/src/main/java/com/zlt/aps/tm/api/domain/entity/TmStock.java
+++ Aps-Api/tm-api/src/main/java/com/zlt/aps/tm/api/domain/entity/TmStock.java
@@ -5,7 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ruoyi.common.core.annotation.Excel;
 import com.ruoyi.common.core.web.domain.BaseEntity;
-import com.zlt.aps.common.core.annotation.ImportValidated;
+import com.zlt.common.annotation.ImportExcelValidated;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -21,44 +21,44 @@ public class TmStock extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
     @Excel(name = "ui.data.column.tm.stock.factoryCode", dictType = "biz_factory_name")
-    @ImportValidated(required = true, isCode = true, maxLength = 50)
+    @ImportExcelValidated(required = true, isCode = true, maxLength = 50)
     @ApiModelProperty(value = "工厂编号", name = "factoryCode")
     @TableField(value = "FACTORY_CODE")
     private String factoryCode;
 
     @JsonFormat(pattern = "yyyy-MM-dd")
     @Excel(name = "ui.data.column.tm.stock.stockDate", width = 30, dateFormat = "yyyy-MM-dd")
-    @ImportValidated(required = true, date = true)
+    @ImportExcelValidated(required = true, date = true)
     @ApiModelProperty(value = "库存日期", name = "stockDate")
     @TableField(value = "STOCK_DATE")
     private Date stockDate;
 
     @Excel(name = "ui.data.column.tm.stock.treadCode")
-    @ImportValidated(required = true, isCode = true, maxLength = 50)
+    @ImportExcelValidated(required = true, isCode = true, maxLength = 50)
     @ApiModelProperty(value = "胎面编码", name = "treadCode")
     @TableField(value = "TREAD_CODE")
     private String treadCode;
 
     @Excel(name = "ui.data.column.tm.stock.stockQty")
-    @ImportValidated(required = true, number = true, min = 0, max = 999999)
+    @ImportExcelValidated(required = true, number = true, min = 0, max = 999999)
     @ApiModelProperty(value = "库存数量", name = "stockQty")
     @TableField(value = "STOCK_QTY")
     private BigDecimal stockQty;
 
     @Excel(name = "ui.data.column.tm.stock.badQty")
-    @ImportValidated(number = true, min = 0, max = 999999)
+    @ImportExcelValidated(number = true, min = 0, max = 999999)
     @ApiModelProperty(value = "不良数量", name = "badQty")
     @TableField(value = "BAD_QTY")
     private BigDecimal badQty;
 
     @Excel(name = "ui.data.column.tm.stock.adjustQty")
-    @ImportValidated(number = true, min = -999999, max = 999999)
+    @ImportExcelValidated(number = true, min = -999999, max = 999999)
     @ApiModelProperty(value = "调整数量", name = "adjustQty")
     @TableField(value = "ADJUST_QTY")
     private BigDecimal adjustQty;
 
     @Excel(name = "ui.common.column.remark")
-    @ImportValidated(maxLength = 500)
+    @ImportExcelValidated(maxLength = 500)
     @ApiModelProperty("备注")
     @TableField("REMARK")
     private String remark;
