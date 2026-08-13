6971ce5cbaef7630362b124f507abe9a23fa1b5b
kf_steve <KF_Steve@zlt.com>
2026-08-13 13:47:39 +0800

    胎面、胎侧，工装限制调整

--- APS-Modules/aps-tm/src/main/java/com/zlt/aps/tm/service/ITmLossSettingService.java
+++ APS-Modules/aps-tm/src/main/java/com/zlt/aps/tm/service/ITmLossSettingService.java
@@ -4,4 +4,11 @@ import com.zlt.aps.tm.api.domain.entity.TmLossSetting;
 import com.zlt.bill.common.service.IDocService;
 
 public interface ITmLossSettingService extends IDocService<TmLossSetting> {
+
+    /**
+     * 校验损耗率基础数据的损耗率字段。
+     *
+     * @param entity 待保存或导入的损耗率配置
+     */
+    void validateLossRate(TmLossSetting entity);
 }
