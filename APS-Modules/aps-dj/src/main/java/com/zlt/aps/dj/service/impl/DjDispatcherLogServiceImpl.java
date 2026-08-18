609a5dd9f2ea8a9a311d5d573cb67e1e32ef1e2a
kf_hak <lwh@zlt.com>
2026-08-18 17:50:50 +0800

    修复垫胶排产日志查询数据不正常的问题

--- APS-Modules/aps-dj/src/main/java/com/zlt/aps/dj/service/impl/DjDispatcherLogServiceImpl.java
+++ APS-Modules/aps-dj/src/main/java/com/zlt/aps/dj/service/impl/DjDispatcherLogServiceImpl.java
@@ -1,12 +1,10 @@
 package com.zlt.aps.dj.service.impl;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.zlt.aps.dj.api.domain.entity.DjDispatcherLog;
 import com.zlt.aps.dj.service.DjDispatcherLogService;
-import com.zlt.bill.common.service.AbstractBillService;
+import com.zlt.bill.common.service.AbstractDocService;
 
 /**
  * 垫胶调度员排程操作日志Service业务层处理
@@ -15,16 +13,11 @@ import com.zlt.bill.common.service.AbstractBillService;
  * @date 2026-06-25
  */
 @Service
-public class DjDispatcherLogServiceImpl extends AbstractBillService<DjDispatcherLog>
-        implements DjDispatcherLogService {
-    @Override
-    public int importData(List<DjDispatcherLog> list, boolean updateSupport, long importLogId) {
-        return 0;
-    }
+public class DjDispatcherLogServiceImpl extends AbstractDocService<DjDispatcherLog> implements DjDispatcherLogService {
 
     @Override
-    protected String getBillTypeCode() {
+    protected String getDocTypeCode() {
         return "";
     }
-    
+
 }
