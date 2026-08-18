a760e1101256af4ac6cfb24b563bef44b6ead794
kf_hak <lwh@zlt.com>
2026-08-18 18:43:35 +0800

    调整垫胶基础资料导入功能没有正确处理校验数据的问题

--- Aps-Api/dj-api/src/main/java/com/zlt/aps/dj/api/domain/entity/DjStock.java
+++ Aps-Api/dj-api/src/main/java/com/zlt/aps/dj/api/domain/entity/DjStock.java
@@ -51,6 +51,15 @@ public class DjStock extends BaseEntity {
     private String endTime;
 
     /**
+     * 垫胶名称
+     */
+    @ApiModelProperty(value = "垫胶名称", position = 30)
+    @Excel(name = "ui.data.column.dj.scheduleResult.paddingName")
+    @ImportValidated(name = "ui.data.column.dj.scheduleResult.paddingName", maxLength = 50, isCode = true)
+    @TableField(value = "MATERIAL_NAME")
+    private String materialName;
+
+    /**
      * 垫胶编号
      */
     @ApiModelProperty(value = "垫胶编号", position = 30)
@@ -58,15 +67,6 @@ public class DjStock extends BaseEntity {
     @ImportValidated(name = "ui.data.column.dj.scheduleResult.paddingCode", required = true, maxLength = 50, isCode = true)
     @TableField(value = "MATERIAL_CODE")
     private String materialCode;
-
-    /**
-     * 垫胶名称
-     */
-    @ApiModelProperty(value = "垫胶名称", position = 30)
-    @Excel(name = "ui.data.column.dj.scheduleResult.paddingName")
-    @ImportValidated(name = "ui.data.column.dj.scheduleResult.paddingName", required = true, maxLength = 50, isCode = true)
-    @TableField(value = "MATERIAL_NAME")
-    private String materialName;
 
     /**
      * 库存量
